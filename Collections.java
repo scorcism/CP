84f3405b400c0000bd3c968a0e1e88b4d9ce762c
scorcism <abhishekpathak1720@gmail.com>
2022-06-23 21:59:41 +0530

    Collections

--- Collections.java
+++ Collections.java
@@ -27,12 +27,8 @@ public class Collections{
      * ArrayClass
     */
 
-    public static void array_List(){
-        
-    }
-
     public static void main(String[] args) {
-    
+        hashMap();
     }
 
 
@@ -40,14 +36,18 @@ public class Collections{
         int[] array = {1,2,3,4,5,6,7,8,9,1,2,3};
         Map<Integer,Integer> hm = new HashMap<>();
         hm.put(1,1);
-        hm.put(2,2);
+        hm.put(2,12);
         hm.put(3,3);
         hm.put(1,1);
 
-        for (Map.Entry<Integer,Integer> data : hm.entrySet()) {
-            System.out.println(data.getKey());
-            System.out.println(data.getValue());
-        }
+        System.out.println("Hello");
+        System.out.println(hm.get(2));
+
+
+        // for (Map.Entry<Integer,Integer> data : hm.entrySet()) {
+        //     System.out.println(data.getKey());
+        //     System.out.println(data.getValue());
+        // }
 
     }
 }
