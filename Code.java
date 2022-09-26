cb74f954ecaaaf27bab912452c0610e4b25058ec
scorcism <abhishekpathak1720@gmail.com>
2022-09-26 12:33:36 +0530

    recusrion

--- Code.java
+++ Code.java
@@ -3,7 +3,7 @@ import java.util.*;
 public class Code {
     public static void main(String[] args) {
    
-        List<List<String>> ans = new ArrayList<>();
+        // List<List<String>> ans = new ArrayList<>();
         
 
     }
