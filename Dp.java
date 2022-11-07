cd70ddd1343f4eddaee984a398c2c16ada91937f
scorcism <abhishekpathak1720@gmail.com>
2022-11-07 17:02:46 +0530

    climbinf stairs

--- Dp.java
+++ Dp.java
@@ -1,3 +1,4 @@
+import java.util.*;
 
 public class Dp {
 
@@ -6,12 +7,44 @@ public class Dp {
         return 10;
     }
 
-    public static void climbStairs(int n){
+
+
+    // Tabulation
+    public static int CS_solve2(int n, int[] dp){
+        dp[0] = 0;
+        dp[1] = 1;
+
+        for(int i = 2; i<= n; i++){
+            dp[i] = dp[i-1]  + dp[i-2];
+        }
+
+        return dp[n];
+    }
+    // Memoization
+    public static int CS_solve1(int n, int[] dp){
+        if(n == 0){
+            return 1;
+        }
+        if(n == 1){
+            return 1;
+        }
+        if(dp[n] != -1){
+            return dp[n];
+        }
         
+        return dp[n] = CS_solve1(n-1,dp) + CS_solve1(n-2,dp);
+    }
+
+    public static int climbStairs(int n){
+        int[] dp = new int[n+1];
+        Arrays.fill(dp,-1);
+        // return CS_solve1(n, dp);
+        return CS_solve2(n, dp);
     }
     
     public static void main(String[] args) {
         int[] nums = {7,5,1};
-        System.out.println(minCoins(39,nums));
+        int n = 44;
+        System.out.println(climbStairs(n));
     }
 }
