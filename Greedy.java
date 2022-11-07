05c5a9a3a8e234643815358d6a22e15b524c929d
scorcism <abhishekpathak1720@gmail.com>
2022-11-07 21:01:09 +0530

    candies shop

--- Greedy.java
+++ Greedy.java
@@ -1,10 +1,42 @@
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Comparator;
-
+import java.util.Arrays;
 
 class Greedy {
 
-    class meeting{
+    static ArrayList<Integer> candyStore(int candies[],int N,int K){
+        Arrays.sort(candies);
+        // As the array is sorted the minimum will be at the start index and the 
+        // max element will be at the last index
+        int mini = 0; // To store the minimum price
+        int buy = 0; // For indixating the values to buy
+        int free = N-1; // For indexing the values to get for free
+
+        while(buy <= free){
+            mini = mini + candies[buy];
+            free = free - K;
+            buy++;
+        }
+
+        int maxi= 0;
+        buy = N-1;
+        free = 0;
+        
+        while(free <= buy){
+            maxi = maxi + candies[buy];
+            buy--;
+            free = free + K;
+        }
+
+        ArrayList<Integer> ans = new ArrayList<>();
+        ans.add(mini);
+        ans.add(maxi);
+
+        return ans;
+    }
+
+    static class meeting{
         int start;
         int end;
         int pos;
@@ -16,7 +48,7 @@ class Greedy {
         }
     }
 
-    class meetingComparator implements Comparator<meeting>{
+    static class meetingComparator implements Comparator<meeting>{
         @Override
         
         public int compare(meeting o1,meeting o2){
@@ -26,7 +58,7 @@ class Greedy {
 
     //Function to find the maximum number of meetings that can
     //be performed in a meeting room.
-    public static int maxMeetings(int start[], int end[], int n)
+    public static void maxMeetings(int start[], int end[], int n)
     {
         ArrayList<meeting> meet = new ArrayList<>();
         for(int i = 0; i< start.length ; i++){
@@ -34,6 +66,7 @@ class Greedy {
         }
         
         meetingComparator mc = new meetingComparator();
+        Collections.sort(meet,mc);
         ArrayList<Integer> answer = new ArrayList<>();
 
         // 1st meeting is added
@@ -55,5 +88,9 @@ class Greedy {
 
     public static void main(String[] args) {
         
+        int[] candies = {3,2,1,4};
+        int N = 4;
+        int K = 2;
+        System.out.println(candyStore( candies, N, K));
     }
 }
