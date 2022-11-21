7e498fce045fb5f63230df83f33943163b96ef82
scorcism <abhishekpathak1720@gmail.com>
2022-11-21 08:45:49 +0530

    BFS

---
+++ BFS.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+public class BFS {
+    
+    public static ArrayList<Integer> bfsOfGraph(int V, ArrayList<ArrayList<Integer>> adj){
+
+        ArrayList<Integer> bfs = new ArrayList<>();
+
+        boolean vis[] = new boolean[V];
+
+        Queue<Integer> q = new LinkedList<>();
+
+        q.add(0);
+        vis[0] = true;
+
+        while(!q.isEmpty()){
+            Integer node  = q.poll();
+            bfs.add(node);
+
+
+            for(Integer it: adj.get(node)){
+                if(vis[it] == false){
+                    vis[it] = true;
+                    q.add(it);
+                }
+            }
+        }
+        return bfs;
+    }
+
+    public static void main(String[] args) {
+        
+    }
+}
