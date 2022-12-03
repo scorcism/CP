e4dce2b7e67e7db1210b08c804285cd70a581609
scorcism <abhishekpathak1720@gmail.com>
2022-12-03 09:39:16 +0530

    read

---
+++ Sum4Operator4.java
@@ -0,0 +1,13 @@
+public class Sum4Operator4 {
+
+
+    /*
+        Takes 4 number and perform operators ( +,-,*,/ )
+        and return if the answer is 10
+    */
+
+    public static void main(String[] args){
+
+
+    }
+}
