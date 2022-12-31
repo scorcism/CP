ef73c6aa8e960fedf6e82d7402aecf4c6a3efeb8
scorcism <abhishekpathak1720@gmail.com>
2022-12-31 10:09:15 +0530

    brute: majority element in array

--- One/Arrays.java
+++ One/Arrays.java
@@ -1,7 +1,28 @@
 class Arrays{
 
 
+    // Majority Elements in an Array
+    static public void mj1(int[] array){
+        int element = 0;
+        int maxCount = Integer.MIN_VALUE;
+
+        for(int i = 0; i< array.length -1; i++){
+            int count = 0;
+            for(int j = 1+1; j< array.length-1; j++){
+                if(array[j] == array[i]){
+                    count++;
+                }
+            }
+            if(count > maxCount){
+                maxCount = count;
+                element = array[i];
+            }
+        }
+        System.out.println(element);
+    }
+
     public static void main(String[] args) {
-        
+        int[] array = {5,4,4,4,4,1,1,1,1,1};
+        mj1(array); 
     }
 }
