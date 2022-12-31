09c46cbf5ea57ae89d2090a1faefef2ca6c27c1d
scorcism <abhishekpathak1720@gmail.com>
2022-12-31 10:46:22 +0530

    Moore's Voting Algorithm

--- One/Arrays.java
+++ One/Arrays.java
@@ -25,9 +25,39 @@ class Arrays{
     public static void mj2(int[] array){
         // after hashmp
     }
+    
+    // majority using moore's voting
+    public static void mj3(int[] array){
+        // after hashmp
+        int ele = array[0];
+        int count = 1;
+
+        for(int i = 1; i< array.length-1; i++){
+            if(ele == array[i]){
+                count++;
+            }else{
+                count--;
+            }
+            if(count == 0){
+                ele = array[i];
+                count = 1;
+            }
+        }
+        int maxCount = 0;
+        for(int j = 0; j< array.length-1; j++){
+            if(array[j] == ele){
+                maxCount++;
+            }
+        }
+        if(maxCount > (array.length)/2){
+            System.out.println(ele);
+        }
+        return ;
+
+    }
 
     public static void main(String[] args) {
-        int[] array = {5,4,4,4,4,1,1,1,1,1};
-        mj1(array); 
+        int[] array = {5,4,44,1};
+        mj3(array); 
     }
 }
