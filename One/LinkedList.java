ccab075015412c58dced17ae3c7341e73f7a1f42
scorcism <abhishekpathak1720@gmail.com>
2023-01-03 19:28:20 +0530

    detect a cycle and return node

--- One/LinkedList.java
+++ One/LinkedList.java
@@ -74,6 +74,23 @@ public class LinkedList {
         }
         return true;
     }
+    
+    // Detect cycle and return the node if not return null
+    public static Node detectCycle(Node head){
+        Node slow = head;
+        Node fast = head;
+
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if(slow == fast){
+                return slow;
+            }
+        }
+
+        return null;
+    }
 
 
     public static void main(String[] args) {
