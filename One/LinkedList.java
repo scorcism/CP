fd14010a3a37611c56bcc87f304ad619242694d8
scorcism <abhishekpathak1720@gmail.com>
2023-01-03 18:45:32 +0530

    middle of a linked list

--- One/LinkedList.java
+++ One/LinkedList.java
@@ -37,6 +37,16 @@ public class LinkedList {
         return newHeadNode;
     }
 
+    private static Node midOfLL(Node head){
+        Node slow = head;;
+        Node fast = head;
+        while(fast != null && fast.next  != null){
+            slow = slow.next;
+            fast = fast.next.next;            
+        }
+        return slow;
+    }
+
 
     public static void main(String[] args) {
         
