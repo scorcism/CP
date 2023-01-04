d3b3b6814af5bbbc0cfff1c1c8985337619e6597
scorcism <abhishekpathak1720@gmail.com>
2023-01-04 15:11:25 +0530

    previous greater element

--- One/StackDS.java
+++ One/StackDS.java
@@ -21,11 +21,28 @@ public class StackDS {
         }
     }
 
-    
+    public static void previousGreaterElement(int[] array){
+        Stack<Integer> s = new Stack<>();
+
+        for(int i = 0; i< array.length; i++){
+            while( !s.isEmpty() && s.peek() <= array[i]){
+                s.pop();
+            }
+            if(s.isEmpty()){
+                System.out.println(-1);
+            }
+            if(!s.isEmpty() && s.peek() > array[i]){
+                System.out.println(s.peek());
+            }
+            s.push(array[i]);
+        }
+    }
+
+
 
     public static void main(String[] args) {
         int array[]  ={4,10,5,8,20,15,3,11};
-        previousSmallerElement(array);        
+        previousGreaterElement(array);        
         
     }
 }
