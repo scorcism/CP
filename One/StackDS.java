42387323ddafe854e1dcdcad6e64c26aa0f89e42
scorcism <abhishekpathak1720@gmail.com>
2023-01-04 14:58:50 +0530

    previous smallest element

--- One/StackDS.java
+++ One/StackDS.java
@@ -1,6 +1,31 @@
+import java.util.Stack;
+
 public class StackDS {
     
+    public static void previousSmallerElement(int[] array){
+        Stack<Integer> s = new Stack<>();
+
+        for(int i = 0; i<array.length; i++){
+            // while stack is not empty and the top element of the stack is not less the the arr[i] keep remoing the top one 
+            while(!s.isEmpty() && s.peek() >= array[i]){
+                s.pop();
+            }
+
+            // if the stack is empty
+            if(s.isEmpty()){
+                System.out.println(-1);
+            }else{
+                System.out.println(s.peek());
+            }
+            s.push(array[i]);
+        }
+    }
+
+    
+
     public static void main(String[] args) {
+        int array[]  ={4,10,5,8,20,15,3,11};
+        previousSmallerElement(array);        
         
     }
 }
