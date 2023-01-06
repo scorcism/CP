4fc595c9a1a44d91502303c9700d164439d4dafe
scorcism <abhishekpathak1720@gmail.com>
2023-01-06 12:04:07 +0530

    flattening a linked list O(1)

--- One/Questions.java
+++ One/Questions.java
@@ -39,6 +39,37 @@ public class Questions {
         return head;
     }
 
+    static NodeF flatLL2(NodeF head){
+        
+        NodeF curr = head;
+        NodeF last = head;
+
+        // move the last pointer to the last of the current list
+        while(last.next != null){
+            last = last.next;
+        }
+        // while the current node is brefore the last node
+        while(curr != last){
+
+            // if the current node has a child node then point ths last ndeo next to the child node
+            if(curr.child != null){
+                last.next= curr.child;
+
+                // move the last to the last element of the current child.
+                NodeF tmp = curr.child;
+                while(last.next != null){
+                    tmp = tmp.next;
+                }
+                last = tmp;
+            }
+            curr = curr.next;
+        }
+
+
+        return curr;
+    }
+
+
     public static void main(String[] args) {
         
     }
