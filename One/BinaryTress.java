95e13d1c6dd87e11e546b720ff8e78bcaf5a1c4d
scorcism <abhishekpathak1720@gmail.com>
2023-01-09 09:41:14 +0530

    edits

--- One/BinaryTress.java
+++ One/BinaryTress.java
@@ -349,6 +349,12 @@ public class BinaryTress {
         return root;
     }
 
+    static int burnTree(Node root){
+        int ans = 0;
+
+        return ans;
+    }
+
     public static void main(String[] args) {
 
         int[] nodes = { 1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1 };
