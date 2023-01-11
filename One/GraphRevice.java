6901885291b70c7fca53f65b407ad092ef300e18
scorcism <abhishekpathak1720@gmail.com>
2023-01-11 20:21:01 +0530

    run bfs

--- One/GraphRevice.java
+++ One/GraphRevice.java
@@ -84,6 +84,21 @@ class GraphRevice{
 
 
     public static void main(String[] args) {
-        
+        ArrayList < ArrayList < Integer >> adj = new ArrayList < > ();
+        for (int i = 0; i < 5; i++) {
+            adj.add(new ArrayList < > ());
+        }
+        adj.get(0).add(1);
+        adj.get(1).add(0);
+        adj.get(0).add(2);
+        adj.get(2).add(0);
+        adj.get(0).add(3);
+        adj.get(3).add(0);
+        adj.get(2).add(4);
+        adj.get(4).add(2);
+
+
+        // ArrayList < Integer > ans = bfs(adj,5);
+        // System.out.println(ans);
     }
 }
