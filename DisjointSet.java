4fa2c0893ac4597ac359beeb60e4eab0afa91fe4
scorcism <abhishekpathak1720@gmail.com>
2023-01-13 07:43:21 +0530

    Disjointset- find && union by rank

---
+++ DisjointSet.java
@@ -0,0 +1,42 @@
+public class DisjointSet {
+    int[] rank, parent;
+    int n;
+
+    DisjointSet(int n) {
+        rank = new int[n];
+        parent = new int[n];
+        this.n = n;
+        for (int i = 0; i < n; i++) {
+            parent[i] = i;
+        }
+    }
+
+    int find(int x) {
+        if (parent[x] == x) {
+            return x;
+        }
+        return find(parent[x]);
+    }
+
+    void union(int a, int b) {
+        int xRoot = find(a);
+        int yRoot = find(b);
+
+        // same parent
+        if (xRoot == yRoot) {
+            return;
+        }
+
+        if(rank[xRoot] < rank[yRoot]){
+            parent[xRoot] = yRoot;
+        }else if (rank[xRoot] > rank[yRoot]){
+            parent[yRoot] = xRoot;
+        }
+        else{
+            parent[yRoot] = xRoot;
+            rank[xRoot] = rank[xRoot] + 1;
+        }
+
+    }
+
+}
