b11bb81c53f1a16110c3fea978af29c08dd691e1
scorcism <abhishekpathak1720@gmail.com>
2023-01-17 20:29:51 +0530

    thread method 2 using runnable interface

--- ThreadWayTwo.java
+++ ThreadWayTwo.java
@@ -1,11 +1,29 @@
-ThreadWayTwo implements Runnable{
-    public void run(){
-        System.out.println("Thread runs...");
-    }
-
-    public static void main(String[] args){
-        ThreadWayOne t1 = new ThreadWayOne();
-        t1.start();
+class MyThreadRunnable1 implements Runnable {
+    public void run() {
+        for (int i = 0; i < 40000; i++) {
+            System.out.println("T1");
+        }
     }
 }
 
+class MyThreadRunnable2 implements Runnable {
+    public void run() {
+        for (int i = 0; i < 40000; i++) {
+
+            System.out.println("T2");
+        }
+    }
+}
+
+class ThreadWayTwo {
+
+    public static void main(String[] args) {
+        MyThreadRunnable1 t1 = new MyThreadRunnable1();
+        Thread th1 = new Thread(t1);
+        MyThreadRunnable2 t2 = new MyThreadRunnable2();
+        Thread th2 = new Thread(t2);
+
+        th1.start();
+        th2.start();
+    }
+}
