3fc0f97ff7119edf0d1b79b53aa19aa451d70154
scorcism <abhishekpathak1720@gmail.com>
2023-01-22 11:37:02 +0530

    mcm tabulation bottom up

--- One/DP.java
+++ One/DP.java
@@ -13,29 +13,48 @@ public class DP {
         System.out.println();
 
     }
-    
-    
-    static int matrixMultiplication(int[] arr, int N){
+
+    static int matrixMultiplication(int[] arr, int N) {
         int[][] dp = new int[N][N];
-        for(int[] d: dp){
+        for (int[] d : dp) {
             Arrays.fill(d, -1);
         }
-        return f(1,N-1,arr,dp);
+        return f(1, N - 1, arr, dp);
     }
 
     private static int f(int i, int j, int[] arr, int[][] dp) {
-        if(i==j) return 0;
+        if (i == j)
+            return 0;
 
-        int mini = (int)1e9;
-        for(int k = i; k< j; k++){
-            int steps = (arr[i-1] * arr[k] * arr[j]) +f(i, k, arr,dp) + f(k+1, j, arr,dp);
+        int mini = (int) 1e9;
+        for (int k = i; k < j; k++) {
+            int steps = (arr[i - 1] * arr[k] * arr[j]) + f(i, k, arr, dp) + f(k + 1, j, arr, dp);
 
-            mini = Math.min(steps, mini); 
+            mini = Math.min(steps, mini);
         }
 
         return mini;
     }
 
+    static int matrixMultiplicationTabulation(int[] arr, int N) {
+        int[][] dp = new int[N][N];
+        for (int i = 0; i < N; i++) {
+            dp[i][i] = 0;
+        }
+        int mini = (int) 1e9;
+
+        for (int i = N - 1; i >= 1; i++) {
+            for (int j = i + 1; j < N; j++) {
+                for (int k = i; j < j; j++) {
+                    int steps = arr[i - 1] * arr[k] * arr[j] + dp[i][k] + dp[k + 1][i];
+                    mini = Math.min(steps, mini);
+                }
+                ;
+                dp[i][j] = mini;
+            }
+        }
+        return dp[1][N-1];
+    }
 
     static int maxSumRectangle(int R, int C, int M[][]) {
         int maxSum = Integer.MIN_VALUE;
