1ad74910e4f8568bf78ececa75f1907cf83ebb15
scorcism <abhishekpathak1720@gmail.com>
2023-01-22 10:12:37 +0530

    MCM memoization top down

--- One/DP.java
+++ One/DP.java
@@ -13,6 +13,29 @@ public class DP {
         System.out.println();
 
     }
+    
+    
+    static int matrixMultiplication(int[] arr, int N){
+        int[][] dp = new int[N][N];
+        for(int[] d: dp){
+            Arrays.fill(d, -1);
+        }
+        return f(1,N-1,arr,dp);
+    }
+
+    private static int f(int i, int j, int[] arr, int[][] dp) {
+        if(i==j) return 0;
+
+        int mini = (int)1e9;
+        for(int k = i; k< j; k++){
+            int steps = (arr[i-1] * arr[k] * arr[j]) +f(i, k, arr,dp) + f(k+1, j, arr,dp);
+
+            mini = Math.min(steps, mini); 
+        }
+
+        return mini;
+    }
+
 
     static int maxSumRectangle(int R, int C, int M[][]) {
         int maxSum = Integer.MIN_VALUE;
