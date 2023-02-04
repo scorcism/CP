04a43f88643e9ca54058d6bcceb0b74908bad4ba
scorcism <abhishekpathak1720@gmail.com>
2023-02-05 01:22:57 +0530

    set zeros matrix

--- CP.java
+++ CP.java
@@ -273,6 +273,119 @@ class General {
     }
 }
 
+class Matrix {
+
+    // set Matrix Zeros
+    static void setZerosBrute(int[][] matrix) {
+        int row = matrix.length;
+        int col = matrix[0].length;
+
+        // assumption if matrix[i][j] > 0
+
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                if (matrix[i][j] == 0) {
+                    int ind = i - 1; // upper row
+                    while (i >= 0) {
+                        if (matrix[ind][j] != 0) {
+                            matrix[ind][j] = -1;
+                        }
+                        ind--;
+                    }
+
+                    ind = i + 1;
+                    while (ind < row) {
+                        if (matrix[ind][j] != 0) {
+                            matrix[ind][j] = -1;
+                        }
+                        ind++;
+                    }
+
+                    ind = j - 1;
+                    while (ind >= 0) {
+                        if (matrix[i][ind] != 0) {
+                            matrix[i][ind] = -1;
+                        }
+                        ind++;
+                    }
+
+                    ind = j + 1;
+                    while (ind < 0) {
+                        if (matrix[i][ind] != 0) {
+                            matrix[i][ind] = -1;
+                        }
+                        ind++;
+                    }
+                }
+            }
+        }
+
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; i < col; j++) {
+                if (matrix[i][j] <= 0) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+
+    }
+
+    static void setZerosBetter(int[][] matrix) {
+        int row = matrix.length;
+        int col = matrix[0].length;
+
+        int[] dummy1 = new int[row];
+        int[] dummy2 = new int[col];
+        Arrays.fill(dummy1, -1);
+        Arrays.fill(dummy2, -1);
+
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                if (matrix[i][j] == 0) {
+                    dummy1[i] = 0;
+                    dummy2[j] = 0;
+                }
+            }
+        }
+
+        for(int i = 0; i < row ; i++){
+            for(int j = 0; j< col ; j++){
+                if(dummy1[i] == 0 || dummy2[j] == 0){
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+
+    }
+
+    static void setZerosOptimal(int[][] matrix) {
+
+        int col0 = 1;
+        int row = matrix.length;
+        int col = matrix[0].length;
+
+        for(int i = 0; i< row; i++){
+            if(matrix[i][0] == 0) col0 = 0;
+            for(int j = 1 ; j< col; j++){
+                if(matrix[i][j] == 0){
+                    matrix[i][0] = matrix[0][j] = 0; 
+                }
+            }
+        }
+        
+        for(int i = row -1; i >= 0 ; i--){
+            for(int j = col -1; j >= 1; j--){
+                if(matrix[i][0]  == 0 || matrix[0][j] == 0){
+                    matrix[i][j] = 0;
+                }
+            }
+            if(col0 == 0){
+                matrix[i][0] = 0;
+            }
+        }
+    }
+}
+
 public class CP {
     // CP Questions
     public static void main(String[] args) {
--- One/Basic.java
+++ One/Basic.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class Basic {
     
     public static void reverseNumber(int n){
@@ -17,7 +19,67 @@ public class Basic {
 
     }
 
+    static void printMatrix(int[][] a){
+        for(int i = 0; i < a.length; i++){
+            for(int j = 0 ; j< a[0].length; j++){
+                System.out.print(a[0][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    static void setZerosBrute(int[][] matrix) {
+        int row = matrix.length;
+        int col = matrix[0].length;
+
+        // assumption if matrix[i][j] > 0
+
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                if(matrix[i][j] == 0){
+                    // iterate in the row and col and mark them with -1
+                    for(int r = i; r < row ; r++){
+                        if(matrix[r][j] != 0){
+                            matrix[r][j] =  -1;
+                        }
+                    }
+                    for(int r = i; r < row ; r++){
+                        if(matrix[r][j] != 0){
+                            matrix[r][j] =  -1;
+                        }
+                    }
+                    for(int c = j; c < col ; c++){
+                        if(matrix[i][c] != 0){
+                            matrix[i][c] =  -1;
+                        }
+                    }
+
+                    // int[] drow = {-1,0,+1,0};
+                    // int[] dcol = {0,1,0,-1};
+                    
+                    // for(int r = i ; r )
+
+                }
+            }
+        }
+
+        for(int i  = 0; i< row; i++){
+            for(int j = 0; j< col ; j++){
+                if(matrix[i][j] == -1){
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+
+        for(int[] a: matrix){
+            System.out.println(Arrays.toString(a));
+        }
+    }
+
     public static void main(String[] args) {
-        reverseNumber(321);
+        // reverseNumber(321);
+        int[][] mat = {{1,2,3},{4,0,6},{7,8,9}};
+        // printMatrix(mat);
+        setZerosBrute(mat);
     }
 }
