6a178416c642a2d8ef8e5de55c7b50bd79056a04
scorcism <abhishekpathak1720@gmail.com>
2023-02-10 06:02:00 +0530

    lcs memo

--- CP.java
+++ CP.java
@@ -962,6 +962,30 @@ class Strings {
         }
         return ans;
     }
+
+
+    static int lcs(String s1, String s2){
+        int len1 = s1.length();
+        int len2 = s2.length();
+
+        int[][] dp = new int[len1][len2];
+
+        return lcsMemo(s1,s2,len1-1,len2-1,dp);
+
+    }
+
+    private static int lcsMemo(String s1, String s2, int ind1, int ind2,int[][] dp) {
+        if(ind1 <0 || ind2 <0){
+            return 0;
+        }
+        if(dp[ind1][ind2] != 0){
+            return dp[ind1][ind2];
+        }
+        if(s1.charAt(ind1) == s2.charAt(ind2)){
+            return dp[ind1][ind2]= 1 + lcsMemo(s1, s2, ind1-1, ind2-1,dp);
+        }
+        return dp[ind1][ind2] =  Math.max(lcsMemo(s1, s2, ind1-1, ind2,dp),lcsMemo(s1, s2, ind1, ind2-1,dp));
+    }
 }
 
 public class CP {
