c7e53ac0f8801eb78d81bf355caf339803c5863f
scorcism <abhishekpathak1720@gmail.com>
2023-02-10 20:34:46 +0530

    lcs tabu

--- CP.java
+++ CP.java
@@ -986,6 +986,35 @@ class Strings {
         }
         return dp[ind1][ind2] =  Math.max(lcsMemo(s1, s2, ind1-1, ind2,dp),lcsMemo(s1, s2, ind1, ind2-1,dp));
     }
+
+    private static int lcsTabu(String s1, String s2){
+        int n = s1.length();
+        int m = s2.length();
+
+        int[][] dp = new int[n+1][m+1];
+
+        // base case
+        // Here we will shift the index
+        for(int i = 0; i<= n; i++){
+            dp[i][0] = 0;
+        }
+
+        for(int j = 0; j<= m; j++){
+            dp[0][j] = 0;
+        }
+
+        for(int i = 1; i<= n ; i++){
+            for(int j = 0;j<= m ; j++){
+                if(s1.charAt(i) == s2.charAt(j)){
+                    dp[i][j] = 1 + dp[i-1][j-1];
+                }else{
+                    dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
+                }
+            }
+        }
+
+        return dp[n][m];
+    }
 }
 
 public class CP {
