397a11e0232464f5bd19549c4b5ece7f915bbe3c
scorcism <abhishekpathak1720@gmail.com>
2023-02-13 07:11:09 +0530

    is same subtree

--- CP.java
+++ CP.java
@@ -1204,6 +1204,43 @@ class trees {
         invertTree(root.left);
         invertTree(root.right);
     }
+
+    public boolean isSubtree(TreeNode root, TreeNode subRoot) {
+        
+        if(subRoot == null){
+            return true; 
+        }
+        if(root == null){
+            return false;
+        }
+        
+        if(issameSubtree(root, subRoot)){
+            return true;
+        }
+
+        return (isSubtree(root.left,  subRoot) || isSubtree(root.right,  subRoot));
+    }
+
+    private boolean issameSubtree(TreeNode root, TreeNode subRoot) {
+        if(root == null && subRoot == null ){
+            return true;
+        }
+
+        if(root == null || subRoot == null){
+            return false;
+        }
+
+        if(root.val != subRoot.val){
+            return false;
+        }
+
+        return ((issameSubtree(root.left, subRoot.left))&&(issameSubtree(root.right, subRoot.right)));
+
+    }
+
+
+
+
 }
 
 public class CP {
