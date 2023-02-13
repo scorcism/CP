c3f5d55dd0ea55ee51c532c9c67bb412c01f83da
scorcism <abhishekpathak1720@gmail.com>
2023-02-13 08:13:36 +0530

    lca

--- CP.java
+++ CP.java
@@ -1238,6 +1238,24 @@ class trees {
 
     }
 
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if(root == null || root == p || root == q){
+            return root;
+        }    
+        TreeNode left =  lowestCommonAncestor(root.left, p, q);
+        TreeNode right =  lowestCommonAncestor(root.right, p, q);
+
+        if(left == null){
+            return  right;
+        }
+        else if(right == null){
+            return  left;
+        }else{
+            return root;
+        }
+    }
+
+
 
 
 
