de07b2e2a2bdc54d86662bfe3136548e6a8e8a64
scorcism <abhishekpathak1720@gmail.com>
2023-02-19 17:26:42 +0530

    tree level order using queue

--- CP2.java
+++ CP2.java
@@ -1,8 +1,56 @@
-public class CP2 {
-    
+import java.util.*;
+import java.util.LinkedList;
 
+
+public class CP2 {
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public List<List<Integer>> levelOrder(TreeNode root) {
+
+        List<List<Integer>> ans = new LinkedList<>();
+        if(root ==null){
+            return ans;
+        }
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(root);
+        while(!q.isEmpty()){
+            // getting the size of the q so that we can take both the value sin one go
+            int size = q.size();
+            List<Integer> l = new ArrayList<>();
+            for(int i  = 0; i< size; i++){
+              if(q.peek().left != null){
+                q.offer(q.peek().left);
+              }
+              if(q.peek().right != null){
+                q.offer(q.peek().right);
+              }
+              l.add(q.poll().val);
+            }
+            ans.add(l);
+        }
+
+        return ans;
+    }
 
     public static void main(String[] args) {
-        
+
     }
 }
