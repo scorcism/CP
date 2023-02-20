3c0f2d1a7c2943df5fcd728dcb32dbcb49bd3df3
scorcism <abhishekpathak1720@gmail.com>
2023-02-20 19:08:05 +0530

    generate pascal triangle single row

--- CP2.java
+++ CP2.java
@@ -10,6 +10,22 @@ public class CP2 {
         // System.out.println(minOperations(6126));
     }
 
+    public List<Integer> getRow(int rowIndex) {
+        int[] arr = new int[rowIndex+1];
+        arr[0] = 1;
+        for(int i = 1; i<rowIndex ; i++){
+            for(int j = i; j>0 ; j--){
+                arr[j] = arr[j] + arr[j-1];
+            }
+        }   
+
+        List<Integer> res = new ArrayList<>();
+        for(int a: arr){
+            res.add(a);            
+        }
+        return res;
+    }
+
     public List<List<Integer>> generate(int numRows) {
         List<List<Integer>> ans = new ArrayList<>();
         List<Integer> curres,prev =null;    
