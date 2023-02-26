a28b8bc1bd0727cba2d4c8e3492c767f455ae7ad
scorcism <abhishekpathak1720@gmail.com>
2023-02-26 15:38:33 +0530

    edit distance using tabulation

--- DailyChallenges.java
+++ DailyChallenges.java
@@ -487,36 +487,32 @@ class DailyChallenges {
     // Feb 26
     // https://leetcode.com/problems/edit-distance/
 
-    public int minDistance(String word1, String word2) {
-        /*
-         * // Get the lcs
-         * suppose we have
-         * horse and ros so lcs of horse and ros is rs which of length 2
-         * so total deletion in horse will be len(horse) - len(lcs) = 5 - 3 = 2
-         * and total inserting in horse will be len(ros) - len(lcs) = 3 -2 = 1
-         */
-        return word1.length() + word2.length() - 2 * lcs(word1, word2);
-    }
+    public int minDistance(String s1, String s2) {
+        // Tabulation method
+        int n = s1.length();
+        int m = s2.length();
+        int[][] dp = new int[n+1][m+1];
 
-    private int lcs(String s1, String s2) {
-        int dp[][] = new int[s1.length()][s2.length()];
-        for (int[] d : dp) {
-            Arrays.fill(d, -1);
+        //0th row and 0th col with store the the indx as it will take ith or jth steps to convert the i charater to j character
+        for(int i  = 0; i<= n; i++){
+            dp[i][0] = i;
         }
-        return lcsmemo(s1, s2, s1.length() - 1, s2.length() - 1, dp);
-    }
+        for(int j = 0; j<=m ; j++){
+            dp[0][j] = j;
+        }
 
-    private int lcsmemo(String s1, String s2, int ind1, int ind2, int[][] dp) {
-        if (ind1 < 0 || ind2 < 0) {
-            return 0;
+        for(int i= 1; i<= n; i++){
+            for(int j = 1; j<= m; j++){
+                if(s1.charAt(i) == s2.charAt(j)){
+                    dp[i][j] = dp[i-1][j-1];
+                }else{
+                    dp[i][j] = 1  + Math.min(Math.min(dp[i-1][j],dp[i][j-1]),dp[i-1][j-1]);
+                }
+            }
         }
-        if (dp[ind1][ind2] != -1) {
-            return dp[ind1][ind2];
-        }
-        if (s1.charAt(ind1) == s2.charAt(ind2)) {
-            return dp[ind1][ind2] = 1 + lcsmemo(s1, s2, ind1 - 1, ind2 - 2, dp);
-        }
-        return dp[ind1][ind2] = Math.max(lcsmemo(s1, s2, ind1 - 1, ind2, dp), lcsmemo(s1, s2, ind1, ind2 - 1, dp));
-    }
 
+        return dp[n][m];
+    }
 }
+
+
