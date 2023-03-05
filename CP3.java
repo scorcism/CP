a71f6a8cf777e058062dfd4fcc1cb66da7cb1371
scorcism <abhishekpathak1720@gmail.com>
2023-03-05 13:13:56 +0530

    1345. Jump Game IV

--- CP3.java
+++ CP3.java
@@ -9,6 +9,7 @@ import java.util.HashSet;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.Comparator;
+import java.util.Queue;
 
 public class CP3 {
     public static void main(String[] args) {
@@ -18,6 +19,45 @@ public class CP3 {
         // System.out.println(strStr(one, two));
     }
 
+    public int minJumps(int[] arr) {
+        int n = arr.length;
+        if (n == 1)
+            return 0;
+
+        Map<Integer, List<Integer>> indices = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            indices.computeIfAbsent(arr[i], k -> new ArrayList<>()).add(i);
+        }
+
+        Queue<Integer> storeIndex = new LinkedList<>();
+        boolean[] visited = new boolean[n];
+        storeIndex.offer(0);
+        visited[0] = true;
+        int steps = 0;
+
+        while (!storeIndex.isEmpty()) {
+            int size = storeIndex.size();
+            while (size-- > 0) {
+                int currIndex = storeIndex.poll();
+                if (currIndex == n - 1)
+                    return steps;
+
+                List<Integer> jumpNextIndices = indices.get(arr[currIndex]);
+                jumpNextIndices.add(currIndex - 1);
+                jumpNextIndices.add(currIndex + 1);
+                for (int jumpNextIndex : jumpNextIndices) {
+                    if (jumpNextIndex >= 0 && jumpNextIndex < n && !visited[jumpNextIndex]) {
+                        storeIndex.offer(jumpNextIndex);
+                        visited[jumpNextIndex] = true;
+                    }
+                }
+                jumpNextIndices.clear();
+            }
+            steps++;
+        }
+        return -1;
+
+    }
 
     public long countSubarraysMethod1(int[] nums, int minK, int maxK) {
         long leftMostMin = -1;
@@ -25,34 +65,34 @@ public class CP3 {
         long rightMax = -1;
         long totalSubArray = 0;
 
-        for(int i = 0; i< nums.length; ++i){
-            if(nums[i] < minK || nums[i] >maxK){
+        for (int i = 0; i < nums.length; ++i) {
+            if (nums[i] < minK || nums[i] > maxK) {
                 leftMostMin = i;
             }
-            if(nums[i]==minK){
+            if (nums[i] == minK) {
                 leftMin = i;
             }
-            if(nums[i]==maxK){
+            if (nums[i] == maxK) {
                 rightMax = i;
             }
-            totalSubArray += Math.max(0L,Math.min(leftMin,rightMax)-leftMostMin);
+            totalSubArray += Math.max(0L, Math.min(leftMin, rightMax) - leftMostMin);
         }
         return totalSubArray;
-        
+
     }
 
     public static int strStr(String haystack, String needle) {
-          if(needle.length() == 0){
+        if (needle.length() == 0) {
             return 0;
         }
-        if(needle.length() > haystack.length()){
+        if (needle.length() > haystack.length()) {
             return -1;
         }
         int i = 0;
-        int j =needle.length();
-        while(j<= haystack.length()){
+        int j = needle.length();
+        while (j <= haystack.length()) {
             // System.out.println(i +" i");
-            if(needle.equals(haystack.substring(i,j))){
+            if (needle.equals(haystack.substring(i, j))) {
                 // System.out.println("i j " + i + " " + j + " ");
                 return i;
             }
@@ -62,28 +102,27 @@ public class CP3 {
         return -1;
     }
 
-    public static int compressM2(char[] chars){
+    public static int compressM2(char[] chars) {
 
         int indexAns = 0;
         int index = 0;
-        while(index < chars.length){
+        while (index < chars.length) {
             char currchar = chars[index];
             int count = 0;
-            while(index< chars.length && chars[index] == currchar){
+            while (index < chars.length && chars[index] == currchar) {
                 index++;
                 count++;
             }
             chars[indexAns++] = currchar;
-            if(count != 1){
-                for(char ch:Integer.toString((count)).toCharArray()){
+            if (count != 1) {
+                for (char ch : Integer.toString((count)).toCharArray()) {
                     chars[indexAns++] = ch;
                 }
             }
         }
 
-
-       return indexAns;
-    } 
+        return indexAns;
+    }
 
     public static int compress(char[] chars) {
         int i = 0;
@@ -94,12 +133,13 @@ public class CP3 {
                 groupLength++;
             }
             chars[res++] = chars[i];
-            if(groupLength > 1){
-                for(char c: Integer.toString(groupLength).toCharArray());
+            if (groupLength > 1) {
+                for (char c : Integer.toString(groupLength).toCharArray())
+                    ;
 
                 chars[res++] = c;
             }
-            i+= groupLength;
+            i += groupLength;
         }
 
         return res;
