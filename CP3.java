589263227cfa98add1854797f492f64e2f21a44b
Abhishek Pathak <abhishekpathak1720@gmail.com>
2023-03-08 20:46:04 +0530

    cocoeating banana

--- CP3.java
+++ CP3.java
@@ -22,6 +22,36 @@ public class CP3 {
         // System.out.println(findKthPositive(arr, k));
     }
 
+    int minEatingSpeed2(int[] piles, int h) {
+        int left = 1;
+        int right = 1000000000;
+        
+        while(left <= right){ // performing binary search
+            int mid = left + (right - left) / 2; // doing in that way, to handle overflow instead of left + right / 2
+            // if koko can eat, mid bananas per hour in less then or equals to h time
+            if(canEatInTime(piles, mid, h)) right = mid - 1; // means decrement our right pointer to optimise better solution 
+            else left = mid + 1; // if not true, increment left pointer
+        }
+        return left; // bcz left pointer hold our optimize k, at the end of BS
+    }
+
+    boolean canEatInTime(int piles[], int k, int h){
+        
+        int hours = 0; // track count of hours
+        for(int pile : piles){
+            // performing claculation, take an example 
+            // k = 4
+            // pile = 10
+
+            // pile / k => 10 / 4 = 2
+            // pile % k => 2, so we need to have one more hour to eat remaining bananas left over as remainder 
+            // hours = 3;
+            int div = pile / k;
+            hours += div;
+            if(pile % k != 0) hours++; // if remainder value is not 0, we need to have an extra hour
+        }
+        return hours <= h;
+
     public int minEatingSpeed(int[] piles, int H) {
         int l = 1, r = 1000000000;
         while (l < r) {
