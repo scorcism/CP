8b56240a155c256d7ec2641a4d7658973c5c528f
Abhishek Pathak <abhishekpathak1720@gmail.com>
2023-03-08 20:45:12 +0530

    cocoeating banana

--- CP3.java
+++ CP3.java
@@ -22,6 +22,19 @@ public class CP3 {
         // System.out.println(findKthPositive(arr, k));
     }
 
+    public int minEatingSpeed(int[] piles, int H) {
+        int l = 1, r = 1000000000;
+        while (l < r) {
+            int m = (l + r) / 2, total = 0;
+            for (int p : piles)
+                total += (p + m - 1) / m;
+            if (total > H)
+                l = m + 1;
+            else
+                r = m;
+        }
+        return l;
+    }
 
     public long minimumTime(int[] time, int totalTrips) {
         // Initialize the search space as the range of possible minimum times required for all buses to complete at least totalTrips trips.
