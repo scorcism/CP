ccd7801fa95c3755c4ef4793be9777fb2104502d
Abhishek Pathak <abhishekpathak1720@gmail.com>
2023-03-11 19:53:33 +0530

    109. Convert Sorted List to Binary Search Tree

--- CP3.java
+++ CP3.java
@@ -24,6 +24,31 @@ public class CP3 {
     }
 
 
+    public TreeNode sortedListToBST(ListNode head) {
+        if(head == null){
+            return null;
+        }        
+        if(head.next == null){
+            return new TreeNode(head.val);
+        }
+
+        ListNode slow = head;
+        ListNode fast = head.next.next;
+
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        
+        TreeNode res = new TreeNode(slow.next.val);
+        ListNode righthalf = slow.next.next;
+        slow.next=  null;
+        res.left = sortedListToBST(head);
+        res.right = sortedListToBST(righthalf);
+        return res;
+
+    }
+
     // https://leetcode.com/problems/linked-list-random-node/description/
     static class SolutionLLRN {
         ArrayList<Integer> list = new ArrayList<>();
