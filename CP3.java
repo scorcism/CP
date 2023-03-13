8068de869a2277765a4e047370d098c662046941
Abhishek Pathak <abhishekpathak1720@gmail.com>
2023-03-13 08:16:35 +0530

    is symmetric bst

--- CP3.java
+++ CP3.java
@@ -23,6 +23,49 @@ public class CP3 {
         // System.out.println(findKthPositive(arr, k));
     }
 
+    public boolean isSymmetricIterative(TreeNode root) {
+        if(root == null){
+            return true;
+        }
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(root.left);
+        q.add(root.right);
+        while(!q.isEmpty()){
+            TreeNode left = q.poll();
+            TreeNode right = q.poll();
+
+            if(left == null && right == null){
+                continue;
+            }
+            if((left ==null && right != null) || (left != null && right == null)){
+                return false;
+            }
+            if(left.val != right.val){
+                return  false;
+            }
+            q.add(left.left);
+            q.add(right.right);
+            q.add(left.right);
+            q.add(right.left);
+        }
+        return true;
+    }
+    public boolean isSymmetric(TreeNode root) {
+        return isSymmetricCall(root.left,root.right);
+    }
+
+    private boolean isSymmetricCall(TreeNode left, TreeNode right) {
+        if(left == null && right == null){
+            return true;
+        }
+        if((left == null && right != null) ||  (left != null && right == null)){
+            return false;
+        }
+        if(left.val != right.val ){
+            return false;
+        }
+        return isSymmetricCall(left.left, right.right) && isSymmetricCall(left.right, right.left);
+    }
 
     public TreeNode sortedListToBST(ListNode head) {
         if(head == null){
