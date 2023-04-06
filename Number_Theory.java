74c635dc06793e40da9f728f37d28f3df7b09356
Abhishek Pathak <abhishekpathak1720@gmail.com>
2023-04-06 11:40:36 +0530

    subset using bitmask

--- Number_Theory.java
+++ Number_Theory.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class Number_Theory {
     public static void main(String[] args) {
         // half in CP4.java
@@ -13,5 +15,27 @@ class NumberTheory{
         b = a ^ b;
         a = a ^ b;
         System.out.println("a: " + a + "b: " + b);
+    }
+
+
+    // Generate subset
+    public static ArrayList<ArrayList<Integer>> subsets(int[] a){
+        ArrayList<ArrayList<Integer>> ans = new ArrayList<>();
+        int n = a.length;
+        int subset_count = (1<<n); // 2^n
+
+        for(int mask = 0; mask < subset_count; mask++){
+            ArrayList<Integer> subset = new ArrayList<>();
+
+            // check for mask and ith value
+            for(int i = 0; i< n; i++){
+                if((mask & (1<<i))!=0){
+                    subset.add(a[i]);
+                }
+            }
+            ans.add(subset);
+        }
+
+        return ans;
     }
 }
