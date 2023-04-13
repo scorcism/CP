8aba7f36bd321593b9b169e6da6266537a912f6a
Abhishek Pathak <abhishekpathak1720@gmail.com>
2023-04-14 04:53:01 +0530

    50A - Domino piling

--- codeforces/CodeForces.java
+++ codeforces/CodeForces.java
@@ -4,27 +4,12 @@ import java.util.Scanner;
 
 public class CodeForces {
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
-        int n;
-        int k;
-        n = sc.nextInt();
-        k = sc.nextInt();
-        int count = 0;
-        int[] ar = new int[n+1];
-        int j = 1;
-        for(int i = 0; i<n; i++){
-            int a = sc.nextInt();
-            ar[j++] = a; 
-        }
-
-        int kth = ar[k];
-
-        // System.out.println(kth + " kth");
-        for(int i = 1;i<ar.length; i++){
-            if(ar[i]>=kth && ar[i] >0){
-                count++;
-            }
-        } 
-        System.out.println(count);
+        
+        int m = sc.nextInt();
+        int n = sc.nextInt();
+        
+        System.out.println((m*n )/2);
     }
 }
