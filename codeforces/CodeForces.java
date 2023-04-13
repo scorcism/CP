88ca73943a3654c996a14aac3ab9a1a9406cf90a
Abhishek Pathak <abhishekpathak1720@gmail.com>
2023-04-14 04:29:56 +0530

    A. Next Round

--- codeforces/CodeForces.java
+++ codeforces/CodeForces.java
@@ -4,20 +4,27 @@ import java.util.Scanner;
 
 public class CodeForces {
     public static void main(String[] args) {
-        int n;
         Scanner sc = new Scanner(System.in);
+        int n;
+        int k;
         n = sc.nextInt();
+        k = sc.nextInt();
         int count = 0;
+        int[] ar = new int[n+1];
+        int j = 1;
         for(int i = 0; i<n; i++){
             int a = sc.nextInt();
-            int b = sc.nextInt();
-            int c = sc.nextInt();
+            ar[j++] = a; 
+        }
 
-            if((a==1 && b==1)|| (b==1 && c==1) || (a==1&&c==1)){
+        int kth = ar[k];
+
+        // System.out.println(kth + " kth");
+        for(int i = 1;i<ar.length; i++){
+            if(ar[i]>=kth && ar[i] >0){
                 count++;
             }
-
-        }
+        } 
         System.out.println(count);
     }
 }
