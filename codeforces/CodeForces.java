8054b88820bf570d298808beb3f5f3b2df07a4dc
Abhishek Pathak <abhishekpathak1720@gmail.com>
2023-04-14 09:19:58 +0530

    A. Bit++

--- codeforces/CodeForces.java
+++ codeforces/CodeForces.java
@@ -4,12 +4,21 @@ import java.util.Scanner;
 
 public class CodeForces {
     public static void main(String[] args) {
-
         Scanner sc = new Scanner(System.in);
-        
-        int m = sc.nextInt();
+       
         int n = sc.nextInt();
         
-        System.out.println((m*n )/2);
+        int number = 0;
+        for(int i = 0; i<n; i++){
+            String op = sc.next();
+            
+            if(op.charAt(1)=='-'){
+                number--;
+            }else{
+                number++;
+            }
+        }
+        System.out.println(number);
+
     }
 }
