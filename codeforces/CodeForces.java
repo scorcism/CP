7610c12f10765dc716b844be0c622d7d1fd2d37d
Abhishek Pathak <abhishekpathak1720@gmail.com>
2023-04-16 00:12:47 +0530

    617A - Elephant

--- codeforces/CodeForces.java
+++ codeforces/CodeForces.java
@@ -1,25 +1,13 @@
 // Abhishek Pathak - scor32k
-// Date: 2023-04-15 23:51:20
+// Date: 2023-04-16 00:00:44
 
 import java.util.Scanner;
 
 public class CodeForces {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-       
-        int k = sc.nextInt();
-        int n = sc.nextInt();
-        int w = sc.nextInt();
-        
-        int t = 0;
-        for(int i = 1; i<=w; i++){
-            t += k*i;
-        }
-        if(t-n <=0){
-            System.out.println(0);
-        }else{
-            System.out.println(t-n);
 
-        }
+        int t = sc.nextInt();
+        System.out.println(t % 5 == 0 ? t / 5 : t / 5 + 1);
     }
 }
