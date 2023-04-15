f2077da713fcc2c20acf29ff3fb619aaf96bba0d
Abhishek Pathak <abhishekpathak1720@gmail.com>
2023-04-15 23:23:37 +0530

    A. Word Capitalization

--- codeforces/CodeForces.java
+++ codeforces/CodeForces.java
@@ -1,8 +1,7 @@
 // Abhishek Pathak - scor32k
-// Date: 2023-04-15 23:00:00
+// Date: 2023-04-15 23:21:11
 
 import java.util.Scanner;
-import java.util.Arrays;
 
 public class CodeForces {
     public static void main(String[] args) {
@@ -10,30 +9,8 @@ public class CodeForces {
        
         String t = sc.next();
         
-        // int[] number_count = new int[4];
-        // for(int i  = 0; i<t.length(); i++){
-        //     if(t.charAt(i) != '+'){
-        //         number_count[t.charAt(i)-'0']++;
-        //     }
-        // }
-        // System.out.println(Arrays.toString(number_count));
-        String ans = "";
-
-        char[] char_number = t.toCharArray();
-        Arrays.sort(char_number);
-        String a  = new String(char_number);
-
-        // System.out.println(a);
-
-        for(int i  = 0 ; i<a.length(); i++){
-            if(a.charAt(i)!='+'){
-                ans+=a.charAt(i);
-                if(i != a.length()-1){
-                    ans+="+";
-                }
-            }
-        }
-        System.out.println(ans);
-
+        char a = t.charAt(0);
+        char aa  = Character.toUpperCase(a);
+        System.out.println(aa+t.substring(1,t.length()));
     }
-}
+}   
