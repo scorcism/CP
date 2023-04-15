fe3745d9acddbc2aa3442d3a44bcbfcc78ad3849
Abhishek Pathak <abhishekpathak1720@gmail.com>
2023-04-15 09:02:31 +0530

    A. Petya and Strings

--- codeforces/CodeForces.java
+++ codeforces/CodeForces.java
@@ -1,16 +1,31 @@
 // Abhishek Pathak - scor32k
+// Date: 2023-04-15 08:54:09
+
 import java.util.Scanner;
 
 public class CodeForces {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
        
-        for(int i = 1; i<= 5; i++){
-            for(int j = 1; j<=5; j++){
-                int x = sc.nextInt();
-                if(x==1){
-                    System.out.println(Math.abs(i-3) + Math.abs(j-3));
-                }
+        String s1 = sc.next();
+        String s2 = sc.next();
+        s1 = s1.toLowerCase();
+        s2 = s2.toLowerCase();
+
+        if(s1.equals(s2)){
+            System.out.println(0);
+            ;
+        }
+
+        for(int i = 0; i < s1.length(); i++){
+            if(s1.charAt(i)==s2.charAt(i)){
+                continue;
+            }else if (s1.charAt(i)<s2.charAt(i)){
+                System.out.println(-1);
+                break;
+            }else {
+                System.out.println(1);
+                break;
             }
         }
     }
