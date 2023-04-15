6406bf84a4d7ffca67fee2aa635053d9052359ec
Abhishek Pathak <abhishekpathak1720@gmail.com>
2023-04-15 08:45:14 +0530

    A. Beautiful Matrix

--- codeforces/CodeForces.java
+++ codeforces/CodeForces.java
@@ -1,24 +1,17 @@
-package codeforces;
-
+// Abhishek Pathak - scor32k
 import java.util.Scanner;
 
 public class CodeForces {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
        
-        int n = sc.nextInt();
-        
-        int number = 0;
-        for(int i = 0; i<n; i++){
-            String op = sc.next();
-            
-            if(op.charAt(1)=='-'){
-                number--;
-            }else{
-                number++;
+        for(int i = 1; i<= 5; i++){
+            for(int j = 1; j<=5; j++){
+                int x = sc.nextInt();
+                if(x==1){
+                    System.out.println(Math.abs(i-3) + Math.abs(j-3));
+                }
             }
         }
-        System.out.println(number);
-
     }
 }
