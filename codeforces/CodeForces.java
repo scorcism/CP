ca092eadf485338166d174cd50d36be5bbd46eda
Abhishek Pathak <abhishekpathak1720@gmail.com>
2023-04-16 00:46:11 +0530

    977A - Wrong Subtraction

--- codeforces/CodeForces.java
+++ codeforces/CodeForces.java
@@ -1,5 +1,5 @@
 // Abhishek Pathak - scor32k
-// Date: 2023-04-16 00:28:38
+// Date: 2023-04-16 00:41:57
 
 import java.util.Scanner;
 
@@ -7,24 +7,18 @@ public class CodeForces {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
        
-        long t = sc.nextLong();
-        
-        int lucky_count = 0;
+        int n = sc.nextInt();
+        int k = sc.nextInt();
 
-        while(t>0){
-            long tmp = t % 10;
-            // System.out.println(tmp);
-            if(tmp == 4 || tmp == 7){
-                lucky_count++;
-            }
-            t /= 10;
-        }
-        // System.out.println(lucky_count);
-        if(lucky_count == 4 || lucky_count == 7){
-            System.out.println("YES");
-        }else{
-            System.out.println("NO");
-        }
         
+        while(k-->0){
+            int tmp = n % 10;
+            if(tmp == 0){
+                n /= 10;
+            }else if(tmp !=0){
+                n = n-1;
+            }
+        }
+        System.out.println(n);
     }
 }
