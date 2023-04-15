9e5507c5df9e74865bbdf547a5fd69cdf5278468
Abhishek Pathak <abhishekpathak1720@gmail.com>
2023-04-16 00:25:18 +0530

    59A - Word

--- codeforces/CodeForces.java
+++ codeforces/CodeForces.java
@@ -1,13 +1,28 @@
 // Abhishek Pathak - scor32k
-// Date: 2023-04-16 00:00:44
+// Date: 2023-04-16 00:19:49
 
 import java.util.Scanner;
 
 public class CodeForces {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
-        int t = sc.nextInt();
-        System.out.println(t % 5 == 0 ? t / 5 : t / 5 + 1);
+       
+        String t = sc.next();
+        
+        int l = 0;
+        int u = 0;
+        for(int i = 0; i<t.length(); i++){
+            if(Character.isLowerCase(t.charAt(i))){
+                l++;
+            }else{
+                u++;
+            }
+        }
+        // System.out.println(l + " " + u);     
+        if(l==u || l>u){
+            System.out.println(t.toLowerCase());
+        }else {
+            System.out.println(t.toUpperCase());
+        }
     }
 }
