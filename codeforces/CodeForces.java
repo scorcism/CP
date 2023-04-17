1241f2fa92005eb835500882143b74b640a38ab6
Abhishek Pathak <abhishekpathak1720@gmail.com>
2023-04-17 05:30:37 +0530

    ordinal

--- codeforces/CodeForces.java
+++ codeforces/CodeForces.java
@@ -1,28 +1,26 @@
 // Abhishek Pathak - scor32k
-// Date: 2023-04-17 04:31:39
+// Date: 2023-04-17 05:23:46
 
 import java.util.Scanner;
 
 public class CodeForces {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
+       
         int t = sc.nextInt();
-
-        for (int i = 0; i < t; i++) {
-            int a = sc.nextInt();
-            int b = sc.nextInt();
-            int c = sc.nextInt();
+        
+        for(int i = 0; i<t; i++){
             int n = sc.nextInt();
 
-            int sum = a + b + c + n;
-
-            if (sum % 3 == 0 && a <= sum / 3 && b <= sum / 3 && c <= sum / 3) {
-                System.out.println("YES");
-            }else{
-                System.out.println("NO");
+            int p;
+            int s;
+            for(p = 1, s=  0; p<=n; s++){
+                p =p / (p%10)*(p%10+1);
+                if(p%10==0){
+                    p++;
+                }
             }
-
+            System.out.println(s);
         }
     }
 }
