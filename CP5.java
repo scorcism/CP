93503974e297f882a07c0ffda0373cd7986ebaec
Abhishek Pathak <abhishekpathak1720@gmail.com>
2023-05-02 16:23:16 +0530

    redo: divide

--- CP5.java
+++ CP5.java
@@ -1,20 +1,36 @@
 public class CP5 {
     
     public static void main(String[] args) {
-        System.out.println(squareOfNumber(15));
+        // System.out.println(squareOfNumber(15));
+        System.out.println(divide(3, 33));
+    }
+
+    public static int divide(int a, int b)
+    {
+        int divident = Math.abs(a);
+        int divisor = Math.abs(b);
+        int res = 0;
+
+        while(divident-divisor>=0){
+            int x = 0; // 2 ^ 0 = 1
+
+            while(divident-(divisor<<1<<x)>=0){
+                x++;
+            }
+            res += 1<<x;
+            divident -= divisor<<x;
+        }
+
+        return (divident>=0)== (divisor>=0) ? res : -res;
     }
 
     public static int squareOfNumber(int n){
-        // Given an integer n, calculate the square of a number without using *, / and pow(). 
-
         int square = 0;
 
         int noOfBits = (int)((Math.log(n)/ Math.log(2)) +1);
         
         for(int i  = 0; i<noOfBits; i++){
             if(((n>>i)&1)==1){
-
-                // square += n * (1<<i);
                 square += n << i;
             }
         }
