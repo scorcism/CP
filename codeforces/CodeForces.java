df9295bdfb570cdb884d97e4ca292308f57a36a9
Abhishek Pathak <abhishekpathak1720@gmail.com>
2023-05-03 21:01:18 +0530

    A - Maximum GCD

---
+++ codeforces/CodeForces.java
@@ -0,0 +1,18 @@
+// Abhishek Pathak - scor32k
+// Date: 2023-05-03 20:42:31
+
+import java.util.Scanner;
+
+public class CodeForces {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+       
+        int t = sc.nextInt();
+        
+        for(int i = 0; i<t; i++){
+            int n = sc.nextInt();
+            System.out.println(n/2);
+            
+        }
+    }
+}
