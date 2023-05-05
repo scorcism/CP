408151a43d7c09e1c9fa88b03e469e2eec2b3c3b
Abhishek Pathak <abhishekpathak1720@gmail.com>
2023-05-05 15:09:57 +0530

    combinarion sum 2

--- RecursionRedo.java
+++ RecursionRedo.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 
@@ -144,7 +145,32 @@ class Subsequences{
 
     }
 
-    
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        List<List<Integer>> ans = new ArrayList<>();
+        Arrays.sort(candidates); // coz we need sorted combinations
+        solvecs2(candidates, target, 0, new ArrayList<Integer>(), ans);
+        return ans;
+    }
+
+    private void solvecs2(int[] candidates, int target, int index, ArrayList<Integer> a, List<List<Integer>> ans) {
+        if(target ==0){
+            ans.add(new ArrayList<>(a));
+            return;
+        }
+
+        // we can choose from any index
+        for(int i = index; i<candidates.length; i++){
+            if(i> index && candidates[i] == candidates[i-1]){
+                continue;
+            }
+            if(candidates[i] > target){
+                break;
+            }
+            a.add(candidates[i]);
+            solvecs2(candidates, target-candidates[i], i+1, a, ans);
+            a.remove(a.size()-1);
+        }
+    }
 
 }
 
