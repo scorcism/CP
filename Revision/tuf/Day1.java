7c759af83018b01ebd2f5d9ff23929d1a8fc20aa
Abhishek Pathak <abhishekpathak1720@gmail.com>
2023-05-09 19:49:07 +0530

    tuf-day1-Pascal's Triangle

--- Revision/tuf/Day1.java
+++ Revision/tuf/Day1.java
@@ -1,5 +1,7 @@
 package Revision.tuf;
 
+import java.util.*;
+
 // 09-05-2023
 public class Day1 {
 
@@ -11,6 +13,26 @@ public class Day1 {
 
     public static void main(String[] args) {
 
+    }
+
+    // 118. Pascal's Triangle
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> ans = new ArrayList<>();
+        List<Integer> curr = null;
+        List<Integer> prev = null;
+        for (int i = 0; i < numRows; i++) {
+            curr = new ArrayList<>();
+            for(int j = 0; j<= i; j++){
+                if(j==0 || j==i){
+                    curr.add(1);
+                }else{
+                    curr.add(prev.get(j-1)+prev.get(j));   
+                }
+            }
+            prev = curr;
+            ans.add(curr);
+        }
+        return ans;
     }
 
     // Set Matrix Zeroes
