ad9c386ecca1a4a8b016e5d365492e99ce9960f7
Abhishek Pathak <abhishekpathak1720@gmail.com>
2023-05-09 09:26:12 +0530

    N queeennnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn optimized

--- Revision/Backtracking.java
+++ Revision/Backtracking.java
@@ -84,4 +84,49 @@ public class Backtracking {
 
         return true;
     }
+
+
+
+    // optimized method
+    public List<List<String>> solveNQueens2(int n) {
+        List<List<String>> ans = new ArrayList<>();
+
+        char[][] board = new char[n][n];
+        for (int j = 0; j < n; j++) {
+            for (int i = 0; i < n; i++) {
+                board[j][i] = '.';
+            }
+        }
+        int[] forRow = new int[n];
+        int[] forLowerDiagonal = new int[2*n-1];
+        int[] forUpperDiagonal = new int[2*n-1];
+
+
+        solveQueens2(0, board, ans, n, forRow, forLowerDiagonal, forUpperDiagonal);
+        return ans;
+    }
+
+    private void solveQueens2(int col, char[][] board, List<List<String>> ans, int n, int[] forRow, int[] forLowerDiagonal, int[] forUpperDiagonal) {
+
+        if(col == board.length){
+            ans.add(contruct(board));
+            return;
+        }
+
+        for(int row  = 0; row < n; row++){
+            if(forRow[row]==0 && forLowerDiagonal[row+col] ==0 && forUpperDiagonal[n-1 + col - row] ==0 ){
+                board[row][col] = 'Q';
+                forRow[row]  = 1;
+                forLowerDiagonal[row+col] = 1;
+                forUpperDiagonal[n-1 + col - row] =1;
+                solveQueens2(col+1, board, ans, n, forRow, forLowerDiagonal, forUpperDiagonal);
+                board[row][col] = '.';
+                forRow[row]  = 0;
+                forLowerDiagonal[row+col] = 0;
+                forUpperDiagonal[n-1 + col - row] =0;
+            }
+        }
+
+    }
+
 }
