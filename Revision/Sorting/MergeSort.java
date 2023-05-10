d4ff250cd50a58b4ea404f749fcd1b6e9f4b83d3
Abhishek Pathak <abhishekpathak1720@gmail.com>
2023-05-10 19:21:50 +0530

    merge sort

---
+++ Revision/Sorting/MergeSort.java
@@ -0,0 +1,57 @@
+package Revision.Sorting;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class MergeSort {
+
+    public static void main(String[] args) {
+        int[] arr = { 3, 1, 2, 4, 1, 5, 2, 6, 4 };
+        int low = 0;
+        int high = arr.length - 1;
+        mergeSort(arr, low, high);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    static void mergeSort(int[] arr, int low, int high) {
+        if (low >= high) { // we have at least one element
+            return;
+        }
+        int mid = (low + high) / 2;
+        mergeSort(arr, low, mid); // for left
+        mergeSort(arr, mid + 1, high); // for right
+        merge(arr, low, mid, high);
+
+    }
+
+    static void merge(int[] arr, int low, int mid, int high) {
+        ArrayList<Integer> tmp = new ArrayList<>();
+        int left = low;
+        int right = mid + 1;
+
+        while (left <= mid && right <= high) {
+            if (arr[left] <= arr[right]) {
+                tmp.add(arr[left]);
+                left++;
+            } else {
+                tmp.add(arr[right]);
+                right++;
+            }
+        }
+
+        while (left <= mid) {
+            tmp.add(arr[left]);
+            left++;
+        }
+        while (right <= high) {
+            tmp.add(arr[right]);
+            right++;
+        }
+
+        for (int i = low; i <= high; i++) {
+            arr[i] = tmp.get(i - low);
+        }
+
+    }
+
+}
