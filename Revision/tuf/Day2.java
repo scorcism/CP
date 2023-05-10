c6e4e42328ab26c52e6370514bd54b78369d95c8
Abhishek Pathak <abhishekpathak1720@gmail.com>
2023-05-10 13:00:07 +0530

    tuf-day2-Merge Sorted Array

--- Revision/tuf/Day2.java
+++ Revision/tuf/Day2.java
@@ -9,8 +9,69 @@ public class Day2 {
 
     }
 
+    // 88. Merge Sorted Array
+    public void merge1(int[] nums1, int m, int[] nums2, int n) {
+        int i = 0;
+        int j = 0;
+        int[] ans = new int[m + n];
+        int k = 0;
+        while (i < m && j < n) {
+            if (nums1[i] <= nums2[j]) {
+                // ith element is small
+                ans[k] = nums1[i];
+                i++;
+                k++;
+            } else {
+                // jth element is small
+                ans[k] = nums2[j];
+                j++;
+                k++;
+            }
+        }
+        // if left arrya get exosted
+        while (i < m) {
+            ans[k] = nums1[i];
+            k++;
+            i++;
+        }
+        // if right array get exosted
+        while (j < n) {
+            ans[k] = nums2[j];
+            k++;
+            j++;
+        }
+        for (int p = 0; p < ans.length; p++) {
+            nums1[p] = ans[p];
+        }
+    }
 
-    
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        // GAP method
+        for (int i = 0; i < n; i++) {
+            nums1[m + i] = nums2[i];
+        }
+        int length = m + n;
+        int gap = length / 2 + length % 2;
+
+        while (gap > 0) {
+            for (int i = 0; i < n + m - gap; i++) {
+                if (nums1[i] > nums1[i + gap]) {
+                    swap(nums1, i, i + gap);
+                }
+            }
+            if (gap == 1) {
+                break;
+            }
+            gap = gap / 2 + gap % 2;
+
+        }
+    }
+
+    static void swap(int[] arr, int a, int b) {
+        int tmp = arr[a];
+        arr[a] = arr[b];
+        arr[b] = tmp;
+    }
 
     // 56. Merge Intervals
     public int[][] merge(int[][] intervals) {
