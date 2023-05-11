cddbd87b291df627d077770f3e6a1aa2a9e2e7b9
Abhishek Pathak <abhishekpathak1720@gmail.com>
2023-05-11 13:21:01 +0530

    tuf-day3-reverse pairs

--- Revision/tuf/Day3.java
+++ Revision/tuf/Day3.java
@@ -8,6 +8,56 @@ public class Day3 {
 
     }
 
+    public int reversePairs(int[] nums) {
+        return mergeSort(nums, 0, nums.length - 1);
+    }
+
+    private int mergeSort(int[] nums, int low, int high) {
+        if (low >= high) {
+            return 0;
+        }
+        int mid = (low + high) / 2;
+        int inv = mergeSort(nums, low, mid);
+        inv += mergeSort(nums, mid + 1, high);
+        inv += merge(nums, low, mid, high);
+        return inv;
+    }
+
+    private int merge(int[] nums, int low, int mid, int high) {
+        int count = 0;
+        int j = mid + 1;
+        for (int i = low; i <= mid; i++) {
+            while (j <= high && nums[i] > (2 * (long) nums[j])) {
+                j++;
+            }
+            count += (j - (mid + 1));
+        }
+
+        ArrayList<Integer> tmp = new ArrayList<>();
+        int left = low;
+        int right = mid + 1;
+
+        while (left <= mid && right <= high) {
+            if (nums[left] <= nums[right]) {
+                tmp.add(nums[left++]);
+            } else {
+                tmp.add(nums[right++]);
+            }
+        }
+
+        while (left <= mid) {
+            tmp.add(nums[left++]);
+        }
+        while (right <= high) {
+            tmp.add(nums[right++]);
+        }
+        for (int i = low; i <= high; i++) {
+            nums[i] = tmp.get(i - low);
+        }
+
+        return count;
+    }
+
     public int uniquePaths(int m, int n) {
 
         return uP1(m, n, 0, 0);
