defe2ff37190d5bc4a1a05bc10b416252d35a9c3
Abhishek Pathak <abhishekpathak1720@gmail.com>
2023-05-12 10:43:42 +0530

    3 sum

--- Revision/tuf/Day4.java
+++ Revision/tuf/Day4.java
@@ -1,5 +1,12 @@
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 public class Day4 {
 
@@ -8,8 +15,117 @@ public class Day4 {
 
     }
 
+    // 3 Sum
+    public List<List<Integer>> threeSum1(int[] nums) {
+        // Brute Force
+        // N^3
+        int n = nums.length;
+        List<List<Integer>> ans = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                for (int k = j + 1; k < n; k++) {
+                    ArrayList<Integer> temp = new ArrayList<>();
+                    if (nums[i] + nums[j] + nums[k] == 0) {
+                        temp.add(nums[i]);
+                        temp.add(nums[j]);
+                        temp.add(nums[k]);
+                    }
+                    if (temp.size() != 0)
+                        ans.add(temp);
+                }
+            }
 
-    
+        }
+        // ans.add(new ArrayList<>(set));
+        return ans;
+    }
+
+    public List<List<Integer>> threeSum2(int[] nums) {
+        // better
+        int n = nums.length;
+        List<List<Integer>> ans = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            HashSet<Integer> set = new HashSet<>();
+            for (int j = i + 1; j < n; j++) {
+                int third_ele = -1 * (nums[i] + nums[j]);
+                if (set.contains(third_ele)) {
+                    List<Integer> temp = new ArrayList<>();
+                    temp.add(nums[i]);
+                    temp.add(nums[j]);
+                    temp.add(nums[third_ele]);
+                    Collections.sort(temp);
+                    set.addAll(temp);
+                }
+            }
+            // ans.addAll(new ArrayList<Integer>(set));
+        }
+        return ans;
+    }
+
+    public List<List<Integer>> threeSum3(int[] nums) {
+        // using 1 fix and 2 pointer
+        int n = nums.length;
+        Arrays.sort(nums);
+        List<List<Integer>> ans = new ArrayList<>();
+        Set<List<Integer>> set = new HashSet<>();
+        for (int i = 0; i < n; i++) {
+            int j = i + 1;
+            int k = n - 1;
+            while (j < k) {
+                int sum = nums[i] + nums[j] + nums[k];
+                if (sum < 0) {
+                    j++;
+                } else if (sum > 0) {
+                    k--;
+                } else {
+                    // found the answer
+                    set.add(Arrays.asList(nums[i], nums[j], nums[k]));
+                    j++;
+                    k--;
+                }
+            }
+
+        }
+        ans.addAll(set);
+        return ans;
+    }
+
+    public List<List<Integer>> threeSum4(int[] nums) {
+        // using 1 fix and 2 pointer
+        int n = nums.length;
+        Arrays.sort(nums);
+        List<List<Integer>> ans = new ArrayList<>();
+        Set<List<Integer>> set = new HashSet<>();
+        for (int i = 0; i < n; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            int low = i + 1;
+            int high = n - 1;
+            int sum = 0 - nums[i];
+            while (low < high) {
+                if (nums[low] + nums[high] == sum) {
+                    set.add(Arrays.asList(nums[i], nums[low], nums[high]));
+                    low++;
+                    high--;
+                    while (low < high && nums[low] == nums[low - 1]) {
+                        low++;
+                    }
+                    while (low < high && nums[high] == nums[high + 1]) {
+                        high--;
+                    }
+                } else if (nums[low] + nums[high] < sum) {
+                    low++;
+                } else {
+                    high++;
+                }
+
+            }
+        }
+        ans.addAll(set);
+        return ans;
+    }
 
     // Two Sum
     public int[] twoSum1(int[] nums, int target) {
@@ -39,6 +155,5 @@ public class Day4 {
         }
         return new int[] { -1, -1 };
     }
-
 
 }
