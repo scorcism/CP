49ed3148e60c9a962bad3ab9bb8020e1bb4ae1d3
Abhishek Pathak <abhishekpathak1720@gmail.com>
2023-05-12 20:32:43 +0530

    tuf-day4-lengthOfLongestSubstring

--- Revision/tuf/Day4.java
+++ Revision/tuf/Day4.java
@@ -17,6 +17,24 @@ public class Day4 {
 
     }
 
+    public int (String s) {
+        // int length = 0;
+        HashMap<Character, Integer> map = new HashMap<>();
+
+        int left = 0, right = 0;
+        int n = s.length();
+        int len = 0;
+        while (right < n) {
+            if (map.containsKey(s.charAt(right))) {
+                left = Math.max(map.get(s.charAt(right)) + 1, left);
+            }
+            map.put(s.charAt(right), right);
+            len = Math.max(len, right - left + 1);
+            right++;
+        }
+        return len;
+    }
+
     // Number of Subarrays with xor K
     static int subarraysWithSumK(ArrayList<Integer> arr, int k) {
         int xr = 0;
