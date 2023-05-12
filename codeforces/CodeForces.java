d22c2e95bd0440efecce90187c914f69a8c18d60
Abhishek Pathak <abhishekpathak1720@gmail.com>
2023-05-12 08:18:08 +0530

    kool

--- codeforces/CodeForces.java
+++ codeforces/CodeForces.java
@@ -1,5 +1,6 @@
 // Abhishek Pathak - scor32k
-// Date: 2023-05-11 19:19:22
+// Date: 2023-05-11 21:51:35
+
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -10,8 +11,6 @@ import java.util.StringTokenizer;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Random;
 
 public class CodeForces {
@@ -19,33 +18,11 @@ public class CodeForces {
         FastScanner fs = new FastScanner(System.in);
         PrintWriter out = new PrintWriter(System.out);
         int T = fs.nextInt();
-
-        for (int tt = 0; tt < T; tt++) {
-            int arrSize = fs.nextInt();
-            HashMap<Integer, Integer> map = new HashMap<>();
-            for (int i = 0; i < arrSize; i++) {
-                int num = fs.nextInt();
-                if (num >= 0) {
-                    map.put(num, map.getOrDefault(num, 0) + 1);
-                }else{
-                    num*=-1;
-                    map.put(num, map.getOrDefault(num, 0) + 1);
-                }
-            }
-            int count = 0;
-            for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
-                int key = entry.getKey();
-                int value = entry.getValue();
-                if (key == 0) {
-                    count++;
-                } else if (value > 1) {
-                    count += 2;
-                } else if (value == 1) {
-                    count += 1;
-                }
-            }
-            System.out.println(count);
+        
+        for(int tt = 0; tt < T; tt++){
+               
         }
+            
         out.close();
     }
 
@@ -78,7 +55,8 @@ public class CodeForces {
                 }
             }
 
-            return tokenizer.nextToken();
+            return tokenizer.nextToken(
+);
         }
 
         public long nextLong() {
@@ -88,23 +66,23 @@ public class CodeForces {
         public int nextInt() {
             return Integer.parseInt(next());
         }
-
+        
         public double nextDouble() {
-            return Double.parseDouble(next());
-        }
-
+             return Double.parseDouble(next());
+         }
+        
         public int[] nextIntArray(int n) {
             int[] a = new int[n];
             for (int i = 0; i < n; i++)
                 a[i] = nextInt();
             return a;
         }
-
+        
         public long[] nextLongArray(int n) {
             long[] a = new long[n];
             for (int i = 0; i < n; i++)
                 a[i] = nextLong();
             return a;
-        }
+        } 
     }
 }
