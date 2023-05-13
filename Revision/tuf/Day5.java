76532958c46f62f54ce054d55b66e5f6cc2d08dd
Abhishek Pathak <abhishekpathak1720@gmail.com>
2023-05-13 12:26:40 +0530

    tuf-day5-add two number linkedlist

--- Revision/tuf/Day5.java
+++ Revision/tuf/Day5.java
@@ -7,6 +7,32 @@ public class Day5 {
 
     
 
+    // 2. Add Two Numbers
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(0);
+        ListNode temp = dummy;
+        int carry = 0;
+        while (l1 != null || l2 != null || carry == 1) {
+            int sum = 0;
+            if (l1 != null) {
+                sum += l1.val;
+                l1 = l1.next;
+            }
+            if (l2 != null) {
+                sum += l2.val;
+                l2 = l2.next;
+            }
+            sum += carry;
+            carry = sum / 10;
+            ListNode newNode = new ListNode(sum % 10);
+            temp.next = newNode;
+            temp = temp.next;
+        }
+
+        return dummy.next;
+
+    }
+
     // 19. Remove Nth Node From End of List
     public ListNode removeNthFromEnd1(ListNode head, int n) {
         if (head == null) {
