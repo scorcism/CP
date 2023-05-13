989c77ea2402a083d79a51d2c77d5bec5125543e
Abhishek Pathak <abhishekpathak1720@gmail.com>
2023-05-13 10:14:47 +0530

    tuf-day5-merge two linkedlist

--- Revision/tuf/Day5.java
+++ Revision/tuf/Day5.java
@@ -5,6 +5,81 @@ public class Day5 {
 
     }
 
+
+    
+
+    public ListNode mergeTwoLists1(ListNode list1, ListNode list2) {
+        if (list1 == null && list2 == null) {
+            return null;
+        }
+        if (list1 == null && list2 != null) {
+            return list2;
+        }
+        if (list1 != null && list2 == null) {
+            return list1;
+        }
+        ListNode dummyNode = new ListNode(0);
+        ListNode d = dummyNode;
+        ListNode h1 = list1;
+        ListNode h2 = list2;
+
+        while (h1 != null && h2 != null) {
+            if (h1.val < h2.val) {
+                d.next = h1;
+                h1 = h1.next;
+
+            } else {
+                d.next = h2;
+                h2 = h2.next;
+            }
+            d = d.next;
+        }
+        while (h1 != null) {
+            d.next = h1;
+            h1 = h1.next;
+            d = d.next;
+        }
+        while (h2 != null) {
+            d.next = h2;
+            h2 = h2.next;
+            d = d.next;
+        }
+
+        return dummyNode.next;
+
+    }
+
+    public ListNode mergeTwoLists2(ListNode list1, ListNode list2) {
+        if (list1 == null) {
+            return list2;
+        }
+        if (list2 == null) {
+            return list1;
+        }
+        // fix the smaller one to list1 ans larget one to list2
+        if (list1.val > list2.val) {
+            ListNode tmp = list1;
+            ;
+            list1 = list2;
+            list2 = tmp;
+        }
+        ListNode res = list1;
+        while (list1 != null && list2 != null) {
+            ListNode tmp = null;
+            while (list1 != null && list1.val <= list2.val) {
+                tmp = list1;
+                list1 = list1.next;
+            }
+            tmp.next = list2;
+
+            // swap
+            ListNode temp = list1;
+            list1 = list2;
+            list2 = temp;
+        }
+        return res;
+    }
+
     // Middle of the linked list
     public ListNode middleNode1(ListNode head) {
         long totalNode = 0;
