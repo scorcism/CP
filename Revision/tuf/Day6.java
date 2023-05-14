56fe39a2c13a854101bafcdaf42f14a05964a563
Abhishek Pathak <abhishekpathak1720@gmail.com>
2023-05-14 16:56:35 +0530

    tuf-day6-Reverse Nodes in k-Group

--- Revision/tuf/Day6.java
+++ Revision/tuf/Day6.java
@@ -8,6 +8,39 @@ public class Day6 {
     }
 
 
+    
+    // Reverse Nodes in k-Group
+    public ListNode reverseKGroup(ListNode head, int k) {
+        if (head == null || k == 1) {
+            return head;
+        }
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+
+        ListNode curr = dummy, nxt = dummy, pre = dummy;
+
+        int count = 0;
+        while (curr.next != null) {
+            curr = curr.next;
+            count++;
+        }
+
+        while (count >= k) {
+            curr = pre.next;
+            nxt = curr.next;
+
+            for (int i = 1; i < k; i++) {
+                curr.next = nxt.next;
+                nxt.next = pre.next;
+                pre.next = nxt;
+                nxt = curr.next;
+            }
+            pre = curr;
+            count -= k;
+        }
+        return dummy.next;
+
+    }
 
     // Linked List Cycle
     public boolean hasCycle1(ListNode head) {
