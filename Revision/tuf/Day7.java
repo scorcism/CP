b87d730c496e49741ab46f27f500ce67b5982b6b
Abhishek Pathak <abhishekpathak1720@gmail.com>
2023-05-15 10:20:49 +0530

    tuf-day7-Clone a Linked List with random and next pointer

--- Revision/tuf/Day7.java
+++ Revision/tuf/Day7.java
@@ -5,8 +5,50 @@ public class Day7 {
 
     }
 
-
     
+
+    // Copy List with Random Pointer
+    public Node copyRandomList(Node head) {
+        // 3 steps
+
+        // First round: make copy of each node,
+        // and link them together side-by-side in a single list.
+
+        Node front = head;
+        Node iter = head;
+
+        while (iter != null) {
+            front = front.next;
+            Node copy = new Node(iter.val);
+            iter.next = copy;
+            copy.next = front;
+            iter = front;
+        }
+
+        // Second round: assign random pointers for the copy nodes.
+        iter = head;
+        while (iter != null) {
+            if (iter.random != null) {
+                iter.next.random = iter.random.next;
+            }
+            iter = iter.next.next;
+        }
+
+        // // Third round: restore the original list, and extract the copy list.
+        iter = head;
+        Node dummy = new Node(0);
+        Node copy = dummy;
+
+        while (iter != null) {
+            front = iter.next.next;
+            copy.next = iter.next;
+            iter.next = front;
+            copy = copy.next;
+            iter = iter.next;
+        }
+
+        return dummy.next;
+    }
 
     // Rotate List
     public ListNode rotateRight(ListNode head, int k) {
@@ -51,6 +93,18 @@ public class Day7 {
         ListNode(int val, ListNode next) {
             this.val = val;
             this.next = next;
+        }
+    }
+
+    class Node {
+        int val;
+        Node next;
+        Node random;
+
+        public Node(int val) {
+            this.val = val;
+            this.next = null;
+            this.random = null;
         }
     }
 }
