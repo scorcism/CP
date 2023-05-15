cd1310f42fbe53ec3abbae1e721458bb9d59a487
Abhishek Pathak <abhishekpathak1720@gmail.com>
2023-05-15 06:57:54 +0530

    bash folder clean up and screate timer script

---
+++ bashScripts/timer.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+ hour=$1
+ min=$2
+ sec=$3
+        while [ $hour -ge 0 ]; do
+                 while [ $min -ge 0 ]; do
+                         while [ $sec -ge 0 ]; do
+                                 echo -ne "$hour:$min:$sec\033[0K\r"
+                                 let "sec=sec-1"
+                                 sleep 1
+                         done
+                         sec=59
+                         let "min=min-1"
+                 done
+                 min=59
+                 let "hour=hour-1"
+         done
+echo "Done for $1 hr $2 minutes and $3 seconds"
