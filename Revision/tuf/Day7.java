787037963a5ed650fa9d85a068ecd04f8d6bda95
Abhishek Pathak <abhishekpathak1720@gmail.com>
2023-05-15 12:35:25 +0530

    tuf-day7-findMaxConsecutiveOnes

--- Revision/tuf/Day7.java
+++ Revision/tuf/Day7.java
@@ -11,8 +11,24 @@ public class Day7 {
 
     }
 
-
     
+    public int findMaxConsecutiveOnes(int[] nums) {
+        int maxans = 0;
+        
+        int count = 0;
+        for(int n: nums){
+            if(n==1){
+                count++;
+            }else{
+                count=0;
+                maxans = Math.max(count,maxans);
+            }
+        }
+
+        return maxans;
+    }
+
+
     public int removeDuplicates1(int[] nums) {
         HashSet<Integer> set = new HashSet<>();
         for (int n : nums) {
