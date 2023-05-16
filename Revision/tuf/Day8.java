a079c38d68235c435ba581f22605089d2fa071c7
Abhishek Pathak <abhishekpathak1720@gmail.com>
2023-05-16 09:42:25 +0530

    Maximum Meetings in One Room

--- Revision/tuf/Day8.java
+++ Revision/tuf/Day8.java
@@ -9,7 +9,46 @@ public class Day8 {
     }
 
 
-    
+
+    public static ArrayList<Integer> maxMeetings(int N, int[] S, int[] F) {
+        ArrayList<meeting> a = new ArrayList<>();
+        ArrayList<Integer> ans = new ArrayList<>();
+
+        for (int i = 0; i < N; i++) {
+            a.add(new meeting(S[i], F[i], i + 1));
+        }
+        Collections.sort(a, new Comparator<meeting>() {
+            public int compare(meeting a, meeting b) {
+                return a.end - b.end;
+            }
+        });
+
+        int endtime = a.get(0).end;
+        ans.add(a.get(0).pos);
+        for (int i = 1; i < N; i++) {
+            if (a.get(i).start > endtime) {
+                ans.add(a.get(i).pos);
+                endtime = a.get(i).end;
+            }
+        }   
+
+        Collections.sort(ans);
+
+        return ans;
+    }
+
+    static class meeting {
+        int start;
+        int end;
+        int pos;
+
+        meeting(int s, int e, int p) {
+            this.start = s;
+            this.end = e;
+            this.pos = p;
+        }
+    }
+
     // N meetings in one room
     public static int maxMeetings(int start[], int end[], int n) {
 
