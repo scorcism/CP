94f15df2e0d56ab718a66c644b6744478a1c4abe
Abhishek Pathak <abhishekpathak1720@gmail.com>
2023-05-17 10:42:47 +0530

    tuf-day9-39. Combination Sum

--- Revision/tuf/Day9.java
+++ Revision/tuf/Day9.java
@@ -10,6 +10,32 @@ public class Day9 {
 
 
 
+    
+    // Combiantion sum
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> ans = new ArrayList<>();
+        List<Integer> a = new ArrayList<>();
+        solvecombinationSum(candidates, 0, target, a, ans);
+        return ans;
+    }
+
+    private void solvecombinationSum(int[] arr, int i, int target, List<Integer> a, List<List<Integer>> ans) {
+        if (i == arr.length) {
+            if (target == 0) {
+                ans.add(new ArrayList<>(a));
+            }
+            return;
+        }
+        if (arr[i] > target) {
+            a.add(arr[i]);
+            // take and not move
+            solvecombinationSum(arr, i, target - arr[i], a, ans);
+            a.remove(a.size() - 1);
+            // not take but index move
+        }
+        solvecombinationSum(arr, i + 1, target, a, ans);
+    }
+
     // 90. Subsets II
     public List<List<Integer>> subsetsWithDup(int[] nums) {
         Arrays.sort(nums);
@@ -21,17 +47,16 @@ public class Day9 {
 
     private void solvesubsetswithdup(int i, int[] nums, List<Integer> a, List<List<Integer>> ans) {
         ans.add(new ArrayList<>(a));
-        
+
         for (int index = i; index < nums.length; index++) {
             if (index != i && nums[index] == nums[index - 1]) {
                 continue;
             }
             a.add(nums[index]);
             solvesubsetswithdup(index + 1, nums, a, ans);
-            a.remove(a.size()-1);
+            a.remove(a.size() - 1);
         }
     }
-
 
     // Subset Sums
     ArrayList<Integer> subsetSums(ArrayList<Integer> arr, int N) {
