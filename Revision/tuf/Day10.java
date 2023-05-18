c266f938029822fb95805d5e14793949eef3c138
Abhishek Pathak <abhishekpathak1720@gmail.com>
2023-05-18 08:16:34 +0530

    tuf-day10-46. Permutations

---
+++ Revision/tuf/Day10.java
@@ -0,0 +1,60 @@
+import java.util.*;
+
+public class Day10 {
+
+    // commit template -> tuf-day10-questionname
+    public static void main(String[] args) {
+
+    }
+
+    // 46. Permutations
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> ans = new ArrayList<>();
+        boolean[] flag = new boolean[nums.length];
+        // solvepermute(nums, new ArrayList<Integer>(), flag, ans);
+        solvepermute2(nums, 0, ans);
+        return ans;
+    }
+
+    private void solvepermute(int[] nums, List<Integer> list, boolean[] flag, List<List<Integer>> ans) {
+        if (list.size() == nums.length) {
+            ans.add(new ArrayList<>(list));
+
+        }
+
+        for (int index = 0; index < flag.length; index++) {
+            if (flag[index] = false) {
+                flag[index] = true;
+                list.add(nums[index]);
+                solvepermute(nums, list, flag, ans);
+                list.remove(list.size());
+                flag[index] = false;
+                list.remove(list.size() - 1);
+            }
+        }
+    }
+
+    private void solvepermute2(int[] nums, int i, List<List<Integer>> ans) {
+        if (i == nums.length) {
+            List<Integer> list = new ArrayList<>();
+            for (int p = 0; p < nums.length; p++) {
+                list.add(nums[p]);
+            }
+            ans.add(new ArrayList<>(list));
+            return;
+        }
+
+        for (int index = i; index < nums.length; index++) {
+            swap(nums, i, index);
+            solvepermute2(nums, i + 1, ans);
+            swap(nums, i, index);
+        }
+    }
+
+    static void swap(int[] arr, int a, int b) {
+        int tmp = arr[a];
+        arr[a] = arr[b];
+        arr[b] = tmp;
+    }
+
+}
