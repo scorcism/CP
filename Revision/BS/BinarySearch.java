d205799ea42766c6e0a99813df5b6b8b2336f70f
Abhishek Pathak <abhishekpathak1720@gmail.com>
2023-05-18 13:38:21 +0530

    BS: reverse order

--- Revision/BS/BinarySearch.java
+++ Revision/BS/BinarySearch.java
@@ -3,7 +3,26 @@ package BS;
 public class BinarySearch {
 
     public static void main(String[] args) {
-        System.out.println(bs(new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 }, 9));
+        // System.out.println(bs(new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 }, 9));
+        System.out.println(reverseArray(new int[] { 9, 8, 7, 6, 5, 4, 3, 2, 1 }, 2));
+    }
+
+    // Reverse sorted array
+    static int reverseArray(int[] arr, int target) {
+        int start = 0;
+        int end = arr.length - 1;
+
+        while (start <= end) {
+            int mid = start +((end - start) >> 1);
+            if (arr[mid] == target) {
+                return mid;
+            } else if (target < arr[mid]) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+        return -1;
     }
 
     static int bs(int[] arr, int target) {
