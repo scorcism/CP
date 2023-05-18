45a1f1e24151833d06a821a3c8850f9eea61bfaf
Abhishek Pathak <abhishekpathak1720@gmail.com>
2023-05-18 14:18:13 +0530

    BS: count of

--- Revision/BS/BinarySearch.java
+++ Revision/BS/BinarySearch.java
@@ -6,14 +6,54 @@ public class BinarySearch {
 
     public static void main(String[] args) {
         int a[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
-        int aa[] = { 1, 2, 3, 4, 4, 4, 7, 8, 9 };
+        int aa[] = { 1, 2, 3, 4, 4, 4, 4, 7, 8, 9 };
         int b[] = { 9, 8, 7, 6, 5, 4, 3, 2, 1 };
         // System.out.println(bs(a , 9));
         // System.out.println(reverseArray(b, 2));
-        System.out.println(Arrays.toString(firstandlast(aa, 2)));
+        // System.out.println(Arrays.toString(firstandlast(aa, 2)));
+        System.out.println(countinSortedArray(aa, 1));
     }
 
-    
+    // count of ele in sorted array
+    static int countinSortedArray(int[] arr, int target) {
+        int count = 0;
+
+        int low = 0;
+        int high = arr.length - 1;
+
+        // for 1st occurace
+        while (low <= high) {
+            // int mid = (low + high) / 2;
+            int mid = low + ((high - low) >> 1);
+
+            if (arr[mid] == target) {
+                count++;
+                high = mid - 1;
+            } else if (target <= arr[mid]) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+
+        low = 0;
+        high = arr.length - 1;
+        // for 1st occurace
+        while (low <= high) {
+            // int mid = (low + high) / 2;
+            int mid = low + ((high - low) >> 1);
+            if (arr[mid] == target) {
+                count++;
+                low = mid + 1;
+            } else if (target <= arr[mid]) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+
+        return count - 1;
+    }
 
     // First and Last occcurance
     static int[] firstandlast(int[] arr, int target) {
