e879fc00b46a1a6adef0b17a01f2800128e969e6
Abhishek Pathak <abhishekpathak1720@gmail.com>
2023-05-18 15:39:29 +0530

    BS: number of times sorted array is rotated

--- Revision/BS/BinarySearch.java
+++ Revision/BS/BinarySearch.java
@@ -8,10 +8,43 @@ public class BinarySearch {
         int a[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
         int aa[] = { 1, 2, 3, 4, 4, 7, 8, 9 };
         int b[] = { 9, 8, 7, 6, 5, 4, 3, 2, 1 };
+        int bb[] = { 6, 7, 8, 9, 2, 3, 4, 5 };
         // System.out.println(bs(a , 9));
         // System.out.println(reverseArray(b, 2));
         // System.out.println(Arrays.toString(firstandlast(aa, 2)));
         // System.out.println(countinSortedArray(aa, 4));
+        System.out.println(numberOfTimes(bb));
+    }
+
+    
+
+    // Number of Times a Sorted array is Rotated
+    static int numberOfTimes(int[] arr) {
+        // Index of min element
+        // coz min element was supposed to be at 0th index but its is shifte by x
+        // position
+        // and we have to return that x value
+        int low = 0;
+        int high = arr.length - 1;
+        while (low <= high) {
+            int mid = low + ((high - low) >> 1);
+            // System.out.println(mid);
+
+            int prev = (mid - 1 + arr.length) % arr.length;
+
+            int next = (mid + 1) % arr.length;
+
+            if (arr[mid] <= arr[prev] && arr[mid] <= arr[next]) {
+                return mid;
+            }
+
+            if (arr[0] <= arr[mid]) {
+                low = mid + 1;
+            } else if (arr[mid] <= arr[arr.length-1]) {
+                high = mid - 1;
+            }
+        }
+        return -1;
     }
 
     // count of ele in sorted array
