5ad86668256279089a6eafb7d6c6aec513834690
Abhishek Pathak <abhishekpathak1720@gmail.com>
2023-05-19 16:21:47 +0530

    tuf-day11-Matrix Median

--- Revision/tuf/Day11.java
+++ Revision/tuf/Day11.java
@@ -9,6 +9,42 @@ public class Day11 {
         // getNthRoot(3, 27);
     }
 
+    // Matrix Median
+    public int findMedian(int[][] arr) {
+        int low = 1;
+        int high = (int) 1e9;
+        int n = arr.length;
+        int m = arr[0].length;
+        while (low <= high) {
+            int mid = low + ((high - low) >> 1);
+            // getting all the values from the matrix which are less then mid
+            int count = 0;
+            for (int i = 0; i < n; i++) {
+                count += countSmallerThanOrEqualToMid(arr[i], m);
+            }
+            if (count <= (n * m) / 2) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return low;
+    }
+
+    private int countSmallerThanOrEqualToMid(int[] row, int m) {
+        int l = 0;
+        int h = row.length - 1;
+        while (l <= h) {
+            int mid = (l + h) >> 1;
+            if (row[mid] <= mid) {
+                l = mid + 1;
+            } else {
+                h = mid - 1;
+            }
+        }
+        return l;
+    }
+
     // The N-th root of an integer
     private static double multiply(double number, int n) {
         double ans = 1.0;
