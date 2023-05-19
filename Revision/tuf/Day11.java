16dba1c6eb3a8022f2c8005d23880c62e83e2478
Abhishek Pathak <abhishekpathak1720@gmail.com>
2023-05-19 18:57:30 +0530

    Search in Rotated Sorted Array

--- Revision/tuf/Day11.java
+++ Revision/tuf/Day11.java
@@ -9,6 +9,33 @@ public class Day11 {
         // getNthRoot(3, 27);
     }
 
+    // 33. Search in Rotated Sorted Array
+    public int search(int[] nums, int target) {
+        int low = 0;
+        int high = nums.length - 1;
+        while (low <= high) {
+            int mid = low + ((high - low) >> 1);
+            if (nums[mid] == target) {
+                return mid;
+            }
+            if (nums[low] <= nums[mid]) {
+                if (nums[low] <= target && nums[mid] > target) {
+                    high = mid - 1;
+                } else {
+                    low = mid + 1;
+                }
+            } else {
+                if (nums[mid] < target && nums[high] >= target) {
+                    low = mid + 1;
+                } else {
+                    high = mid - 1;
+                }
+            }
+        }
+
+        return -1;
+    }
+
     public int singleNonDuplicate(int[] nums) {
         int low = 0;
         int high = nums.length - 2;
