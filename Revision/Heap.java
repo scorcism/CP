50134c2e50c7803a8e42bca9e2f1f7a67929e35c
Abhishek Pathak <abhishekpathak1720@gmail.com>
2023-05-20 21:34:25 +0530

    heap: findClosestElements

--- Revision/Heap.java
+++ Revision/Heap.java
@@ -6,20 +6,39 @@ public class Heap {
         System.out.println(kthSmallest(arr, 3));
     }
 
+    public List<Integer> findClosestElements(int[] arr, int k, int x) {
+        List<Integer> ans = new ArrayList<>();
 
-    // Sort a nearly sorted (or K sorted) array
-    //Function to return the sorted array.
-    ArrayList <Integer> nearlySorted(int arr[], int num, int k)
-    {
-        ArrayList<Integer> ans = new ArrayList<>();
         PriorityQueue<Integer> pq = new PriorityQueue<>();
         for(int n: arr){
-            pq.add(n);
-            if(pq.size() > k){
-                ans.add(pq.poll());
-            } 
+            if(k > 0){
+                pq.offer(n);
+                k--;
+            }else if(Math.abs(pq.peek()-x )> Math.abs(n-x)){
+                pq.poll();
+                pq.offer(n);
+            }
         }
+        
         while(!pq.isEmpty()){
+            ans.add(pq.poll());
+        }
+
+        return ans;
+    }
+
+    // Sort a nearly sorted (or K sorted) array
+    // Function to return the sorted array.
+    ArrayList<Integer> nearlySorted(int arr[], int num, int k) {
+        ArrayList<Integer> ans = new ArrayList<>();
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for (int n : arr) {
+            pq.add(n);
+            if (pq.size() > k) {
+                ans.add(pq.poll());
+            }
+        }
+        while (!pq.isEmpty()) {
             ans.add(pq.poll());
         }
         return ans;
