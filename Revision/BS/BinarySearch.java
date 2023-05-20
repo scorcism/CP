471ecff4cb39983a7725ce06735b995e4555b589
Abhishek Pathak <abhishekpathak1720@gmail.com>
2023-05-20 12:09:45 +0530

    BS: Searching in a Nearly Sorted Array

--- Revision/BS/BinarySearch.java
+++ Revision/BS/BinarySearch.java
@@ -1,6 +1,6 @@
 package BS;
 
-import java.util.Arrays;
+import java.util.*;
 
 public class BinarySearch {
 
@@ -9,14 +9,47 @@ public class BinarySearch {
         int aa[] = { 1, 2, 3, 4, 4, 7, 8, 9 };
         int b[] = { 9, 8, 7, 6, 5, 4, 3, 2, 1 };
         int bb[] = { 6, 7, 8, 9, 2, 3, 4, 5 };
+        int c[] = { 5, 10, 30, 20, 40 };
         // System.out.println(bs(a , 9));
         // System.out.println(reverseArray(b, 2));
         // System.out.println(Arrays.toString(firstandlast(aa, 2)));
         // System.out.println(countinSortedArray(aa, 4));
-        System.out.println(numberOfTimes(bb));
+        // System.out.println(numberOfTimes(bb));
+        // System.out.println(nearlySorted(c,20));
+
+
     }
 
     
+
+    // Searching in a Nearly Sorted Array
+    static int nearlySorted(int[] arr, int target) {
+        int low = 0;
+        int high = arr.length - 1;
+
+        while (low <= high) {
+            int mid = low +((high - low)>>1);
+
+            if(target == arr[mid] ){
+                return mid;
+            }
+            if(mid-1 >= low && target == arr[mid-1] ){
+                return mid-1;
+            }
+            if(mid+1 <= high && target == arr[mid+1] ){
+                return mid+1;
+            }
+            else if(target < mid){
+                high = mid -2;
+            }else{
+                low = mid+2;
+            }
+        }
+        return -1;
+
+    }
+
+    // Find an Element in a Rotated Sorted Array
 
     // Number of Times a Sorted array is Rotated
     static int numberOfTimes(int[] arr) {
@@ -40,7 +73,7 @@ public class BinarySearch {
 
             if (arr[0] <= arr[mid]) {
                 low = mid + 1;
-            } else if (arr[mid] <= arr[arr.length-1]) {
+            } else if (arr[mid] <= arr[arr.length - 1]) {
                 high = mid - 1;
             }
         }
