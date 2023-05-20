1f9b4b45ae146b2eb7d50bf0e8137d614a3c66bf
Abhishek Pathak <abhishekpathak1720@gmail.com>
2023-05-20 12:42:54 +0530

    BS: Find Floor of an element in a Sorted Array

--- Revision/BS/BinarySearch.java
+++ Revision/BS/BinarySearch.java
@@ -5,7 +5,7 @@ import java.util.*;
 public class BinarySearch {
 
     public static void main(String[] args) {
-        int a[] = { 1, 2, 3, 4, 6, 7, 8, 9 };
+        int a[] = { 1, 2, 3, 4, 7, 8, 9 };
         int aa[] = { 1, 2, 3, 4, 4, 7, 8, 9 };
         int b[] = { 9, 8, 7, 6, 5, 4, 3, 2, 1 };
         int bb[] = { 6, 7, 8, 9, 2, 3, 4, 5 };
@@ -16,7 +16,27 @@ public class BinarySearch {
         // System.out.println(countinSortedArray(aa, 4));
         // System.out.println(numberOfTimes(bb));
         // System.out.println(nearlySorted(c,20));
-        System.out.println(findFloow(a, 5));
+        System.out.println(findCeil(a, 5));
+    }
+
+    // Find Floor of an element in a Sorted Array
+    static int findCeil(int[] arr, int target) {
+        int low = 0;
+        int high = arr.length - 1;
+        int res = -1;
+        while (low <= high) {
+            int mid = low + ((high - low) >> 1);
+            if (arr[mid] == target) {
+                res = arr[mid];
+            }
+            if (arr[mid] < target) {
+                low = mid + 1;
+            } else {
+                res = arr[mid];
+                high = mid - 1;
+            }
+        }
+        return res;
     }
 
     // Find Floor of an element in a Sorted Array
@@ -27,7 +47,7 @@ public class BinarySearch {
         while (low <= high) {
             int mid = (low + high) / 2;
             if (arr[mid] == target) {
-                res = mid;
+                res = arr[mid];
             }
             if (arr[mid] < target) {
                 res = arr[mid];
