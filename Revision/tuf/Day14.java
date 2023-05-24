47819481bb49145b1fddb44dda518a23766f96ee
Abhishek Pathak <abhishekpathak1720@gmail.com>
2023-05-24 09:38:07 +0530

    tuf-day14-LFU Cache

--- Revision/tuf/Day14.java
+++ Revision/tuf/Day14.java
@@ -5,7 +5,67 @@ public class Day14 {
     // commit template -> tuf-day14-questionName
     public static void main(String[] args) {
         int[] A = { 3, 2, 1 };
-        System.out.println(Arrays.toString(prevSmaller(A)));
+        // System.out.println(Arrays.toString(prevSmaller(A)));
+    }
+
+    // LFU Cache
+    static class LRUCache {
+
+        class Node {
+            Node prev, next;
+            int key, value;
+
+            Node(int _key, int _value) {
+                this.key = _key;
+                this.value = _value;
+            }
+        }
+
+        Node head = new Node(0, 0);
+        Node tail = new Node(0, 0);
+        Map<Integer, Node> map = new HashMap();
+        int capacity;
+
+        public LRUCache(int _capacity) {
+            capacity = _capacity;
+            head.next = tail;
+            tail.prev = head;
+        }
+
+        private void remove(Node node) {
+            map.remove(node.key);
+            node.prev.next = node.next;
+            node.next.prev = node.prev;
+        }
+
+        private void insert(Node node) {
+            map.put(node.key, node);
+            node.next = head.next;
+            node.next.prev = node;
+            head.next = node;
+            node.prev = head;
+        }
+
+        public int get(int key) {
+            if (map.containsKey(key)) {
+                Node node = map.get(key);
+                remove(node);
+                insert(node);
+                return node.value;
+            } else {
+                return -1;
+            }
+        }
+
+        public void put(int key, int value) {
+            if (map.containsKey(key)) {
+                remove(map.get(key));
+            }
+            if (map.size() == capacity) {
+                remove(tail.prev);
+            }
+            insert(new Node(key, value));
+        }
     }
 
     static public int[] prevSmaller(int[] A) {
