fb0652cdb2ae6fcf07b1f14e5c4f904677d74d5e
Abhishek Pathak <abhishekpathak1720@gmail.com>
2023-05-25 10:50:19 +0530

    tuf-day15-// 13. Roman to Integer

--- Revision/tuf/Day15.java
+++ Revision/tuf/Day15.java
@@ -7,6 +7,30 @@ class Day15 {
     public static void main(String[] args) {
 
     }
+    
+
+    // 13. Roman to Integer
+    public int romanToInt(String s) {
+        // Move from largest to smallest: add them up
+        // smaller before larger: subtract smaller
+        HashMap<Character, Integer> map = new HashMap<>();
+        map.put('I', 1);
+        map.put('V', 5);
+        map.put('X', 10);
+        map.put('L', 50);
+        map.put('C', 100);
+        map.put('D', 500);
+        map.put('M', 1000);
+        int ans = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (i + 1 < s.length() && map.get(s.charAt(i)) < map.get(s.charAt(i + 1))) {
+                ans -= map.get(s.charAt(i));
+            } else {
+                ans += map.get(s.charAt(i));
+            }
+        }
+        return ans;
+    }
 
     // 5. Longest Palindromic Substring
     public String longestPalindrome(String s) {
