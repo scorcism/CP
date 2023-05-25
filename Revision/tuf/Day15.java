64c6158e0324bd410f6d336dac0112d1b6d9fe14
Abhishek Pathak <abhishekpathak1720@gmail.com>
2023-05-25 12:20:15 +0530

    tuf-day15-8. String to Integer (atoi)

--- Revision/tuf/Day15.java
+++ Revision/tuf/Day15.java
@@ -8,6 +8,105 @@ class Day15 {
 
     }
 
+
+    
+    // 8. String to Integer (atoi)
+    public int myAtoi(String str) {
+
+        final int len = str.length();
+
+        if (len == 0) {
+            return 0;
+        }
+
+        int index = 0;
+
+        // skipping white spaces
+        while (index < len && str.charAt(index) == ' ') {
+            ++index;
+        }
+
+        boolean isNegative = false;
+
+        // to handle sign cases
+        if (index < len) {
+
+            if (str.charAt(index) == '-') {
+                isNegative = true;
+                ++index;
+            } else if (str.charAt(index) == '+') {
+                ++index;
+            }
+
+        }
+
+        int result = 0;
+
+        // converting digit(in character form) to integer form
+        // iterate until non-digit character is not found or we can say iterate till
+        // found character is a digit
+        while (index < len && isDigit(str.charAt(index))) {
+
+            /*
+             * str.charAt(index) - '0' is to convert the char digit into int digit eg: '5' -
+             * '0' --> 5
+             * or else it will store the ASCII value of 5 i.e. 53,
+             * so we do 53(ASCII of 5) - 48(ASCII of 0(zero)) to get 5 as int
+             */
+            int digit = str.charAt(index) - '0';
+
+            // to avoid integer overflow
+            if (result > (Integer.MAX_VALUE / 10) || (result == (Integer.MAX_VALUE / 10) && digit > 7)) {
+                return isNegative ? Integer.MIN_VALUE : Integer.MAX_VALUE;
+            }
+
+            // adding digits at their desired place-value
+            result = (result * 10) + digit;
+
+            ++index;
+        }
+
+        return isNegative ? -result : result;
+        /*
+         * Let's understand what
+         * if(result > (Integer.MAX_VALUE / 10) || (result == (Integer.MAX_VALUE / 10)
+         * && digit > 7)){
+         * return isNegative ? Integer.MIN_VALUE : Integer.MAX_VALUE;
+         * }
+         * means in JAVA? You will be able to figure out this statement easily for C++
+         * code too.
+         * 
+         * result > (Integer.MAX_VALUE / 10) means:
+         * Suppose,
+         * result = 214748365(9 digits)
+         * Integer.MAX_VALUE = 2147483647(10 digits) and Integer.MAX_VALUE / 10 =
+         * 214748364(9 digits)
+         * Here, it is clearly evident that result > Integer.MAX_VALUE / 10( i.e.
+         * 214748365 > 214748364) and if we try to add even 0(zero) in result
+         * 214748365(9 digits), the number will become 2147483650(10 digits) which is
+         * obviously greater than 2147483647(Integer.MAX_VALUE which is of 10 digits).
+         * So even before adding 0(zero) or any other digit, we return the
+         * Integer.MAX_VALUE or Integer.MIN_VALUE, according to the sign case, in order
+         * to avoid integer overflow.
+         * 
+         * And, result == (Integer.MAX_VALUE / 10) && digit > 7 means:
+         * Suppose,
+         * result = 214748364(9 digits), and
+         * Integer.MAX_VALUE / 10 = 214748364(9 digits)
+         * Now, if the result is equal to the Integer.MAX_VALUE / 10 (214748364 ==
+         * 214748364) and the digit is greater than 7 i.e. digit > 7 and if we try to
+         * add 8(assume the digit greater than 7 to be 8) to the result, then the number
+         * will become 2147483648(10 digits), which will result in integer overflow. So,
+         * even before adding the digit which is greater than 7, we return the
+         * Integer.MAX_VALUE or Integer.MIN_VALUE, according to the sign case, to avoid
+         * integer overflow.
+         */
+    }
+
+    private boolean isDigit(char ch) {
+        return ch >= '0' && ch <= '9';
+    }
+
     // 12. Integer to Roman
     public String intToRoman(int num) {
         int[] val = { 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 };
