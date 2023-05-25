b5a01521c9f3a617b500385896b4eedabb53aa9d
Abhishek Pathak <abhishekpathak1720@gmail.com>
2023-05-25 10:19:07 +0530

    tuf-day15-5. Longest Palindromic Substring

--- Revision/tuf/Day15.java
+++ Revision/tuf/Day15.java
@@ -8,6 +8,45 @@ class Day15 {
 
     }
 
+    // 5. Longest Palindromic Substring
+    public String longestPalindrome(String s) {
+        int len = s.length();
+
+        int resLen = 0;
+        String ans = "";
+        int res_l = 0;
+        int res_r = 0;
+        for (int i = 0; i < len; i++) {
+            int left = i;
+            int right = i;
+
+            while (left >= 0 && right < len && s.charAt(left) == s.charAt(right)) {
+                if (right - left + 1 > resLen) {
+                    resLen = right - left + 1;
+                    // ans = s.substring(left, right+1);
+                    res_l = left;
+                    res_r = right + 1;
+                }
+                left--;
+                right++;
+            }
+            left = i;
+            right = i + 1;
+
+            while (left >= 0 && right < len && s.charAt(left) == s.charAt(right)) {
+                if (right - left + 1 > resLen) {
+                    resLen = right - left + 1;
+                    // ans = s.substring(left, right+1);
+                    res_l = left;
+                    res_r = right + 1;
+                }
+                left--;
+                right++;
+            }
+        }
+        return s.substring(res_l, res_r);
+    }
+
     // Reverse Words in a String
     public String reverseWords1(String s) {
         Stack<String> stack = new Stack<>();
@@ -32,7 +71,7 @@ class Day15 {
         int i = s.length() - 1;
         String sb = "";
         while (i >= 0) {
-            
+
             while (i >= 0 && s.charAt(i) == ' ')
                 i--;
 
@@ -41,14 +80,14 @@ class Day15 {
             while (i >= 0 && s.charAt(i) != ' ')
                 i--;
 
-            if( i<0){
+            if (i < 0) {
                 break;
             }
 
             if (sb.isEmpty()) {
-                sb = sb.concat(s.substring(i+1, j+1));
+                sb = sb.concat(s.substring(i + 1, j + 1));
             } else {
-                sb = sb.concat(" " + s.substring(i+1, j+1));
+                sb = sb.concat(" " + s.substring(i + 1, j + 1));
             }
         }
         return sb;
