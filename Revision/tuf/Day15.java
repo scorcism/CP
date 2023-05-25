3e141f1d11ccf6e57e1f533fbe915f417d5c5de7
Abhishek Pathak <abhishekpathak1720@gmail.com>
2023-05-25 11:19:47 +0530

    tuf-day15-12. Integer to Roman

--- Revision/tuf/Day15.java
+++ Revision/tuf/Day15.java
@@ -7,7 +7,23 @@ class Day15 {
     public static void main(String[] args) {
 
     }
-    
+
+    // 12. Integer to Roman
+    public String intToRoman(int num) {
+        int[] val = { 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 };
+        String[] symbols = { "M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I" };
+        StringBuilder sb = new StringBuilder();
+        while (num > 0) {
+            for (int i = 0; i < val.length; i++) {
+                if (num >= val[i]) {
+                    sb.append(symbols[i]);
+                    num -= val[i];
+                    break;
+                }
+            }
+        }
+        return sb.toString();
+    }
 
     // 13. Roman to Integer
     public int romanToInt(String s) {
