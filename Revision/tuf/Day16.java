63519d8563d53d88ca690984912c1074012822a7
Abhishek Pathak <abhishekpathak1720@gmail.com>
2023-05-26 12:36:33 +0530

    tuff-day16-165. Compare Version Numbers

--- Revision/tuf/Day16.java
+++ Revision/tuf/Day16.java
@@ -8,6 +8,39 @@ public class Day16 {
 
     }
 
+    // 165. Compare Version Numbers
+    public int compareVersion(String version1, String version2) {
+        int i = 0;
+        int j = 0;
+        int len1 = version1.length();
+        int len2 = version2.length();
+        int n1 = 0;
+        int n2 = 0;
+
+        while (i < len1 || j < len2) {
+            n1 = 0;
+            n2 = 0;
+            while (i < len1 && version1.charAt(i) != '.') {
+                n1 = (n1 * 10) + version1.charAt(i) - '0';
+                i++;
+            }
+            while (j < len2 && version2.charAt(j) != '.') {
+                n2 = (n2 * 10) + version2.charAt(j) - '0';
+                j++;
+            }
+            if (n1 > n2) {
+                return 1;
+            }
+            if (n1 < n2) {
+                return -1;
+            }
+            i++;
+            j++;
+        }
+
+        return 0;
+    }
+
     // 38. Count and Say
     public String countAndSay(int n) {
         if (n == 1) {
