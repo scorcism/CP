55ab4e02acaf9ce98ec55ec10abcb233a1d51af8
Abhishek Pathak <abhishekpathak1720@gmail.com>
2023-05-26 12:20:13 +0530

    tuff-day16-38. Count and Say

--- Revision/tuf/Day16.java
+++ Revision/tuf/Day16.java
@@ -8,6 +8,33 @@ public class Day16 {
 
     }
 
+    // 38. Count and Say
+    public String countAndSay(int n) {
+        if (n == 1) {
+            return "1";
+        }
+        if (n == 2) {
+            return "11";
+        }
+        String s = "11";
+        for (int i = 3; i <= n; i++) {
+            String t = "";
+            s = s + "$";
+            int c = 1;
+            for (int j = 1; j < s.length(); j++) {
+                if (s.charAt(j) != s.charAt(j - 1)) {
+                    t = t + Integer.toString(c);
+                    t = t + s.charAt(j - 1);
+                    c = 1;
+                } else {
+                    c++;
+                }
+            }
+            s = t;
+        }
+        return s;
+    }
+
     // 242. Valid Anagram
     public boolean isAnagram(String s, String t) {
         if (t.length() > s.length() || s.length() > t.length()) {
