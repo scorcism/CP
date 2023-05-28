54d8752006c3da2e80b6c4985b24494d3aca3cbf
Abhishek Pathak <abhishekpathak1720@gmail.com>
2023-05-28 14:15:24 +0530

    tuf-daLowest Common Ancestor of a Binary Tree

--- Revision/tuf/Day18.java
+++ Revision/tuf/Day18.java
@@ -6,6 +6,25 @@ public class Day18 {
 
     }
 
+    
+    // Lowest Common Ancestor of a Binary Tree
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null || root == p || root == q) {
+            return root;
+        }
+
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+        if (left == null) {
+            return right;
+        }
+        if (right == null) {
+            return left;
+        } else {
+            return root;
+        }
+    }
+
     // Balanced Binary Tree
     public boolean isBalanced(TreeNode root) {
         return usingHeight(root) != -1;
