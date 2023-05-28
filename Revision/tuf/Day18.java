103566b8aaea8f1cb6af1ea722c96a7a3e4b52ee
Abhishek Pathak <abhishekpathak1720@gmail.com>
2023-05-28 14:56:06 +0530

    tuf-day18-Binary Tree Zigzag Level Order Traversal

--- Revision/tuf/Day18.java
+++ Revision/tuf/Day18.java
@@ -6,6 +6,41 @@ public class Day18 {
 
     }
 
+    // Binary Tree Zigzag Level Order Traversal
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+
+        List<List<Integer>> ans = new ArrayList<>();
+        if (root == null) {
+            return ans;
+        }
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(root);
+        boolean leftToRight = true;
+        while (!q.isEmpty()) {
+            int size = q.size();
+            List<Integer> a = new ArrayList<>();
+            for (int i = 0; i < size; i++) {
+                TreeNode node = q.poll();
+                // int index = (leftToRight) ? i : (size - 1 - i);
+                if(leftToRight){
+                    a.add(node.val);
+                }else{
+                    a.add(0,node.val);
+                }
+                // a.add(index, node.val);
+                if(node.left != null){
+                    q.add(node.left);
+                }
+                if(node.right!= null){
+                    q.add(node.right);
+                }
+            }
+            leftToRight = !leftToRight;
+            ans.add(a);
+        }
+        return ans;
+    }
+
     public boolean isSameTree(TreeNode p, TreeNode q) {
         if (p == null && q == null) {
             return true;
