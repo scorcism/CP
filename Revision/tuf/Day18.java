760b15ee8f1860f9752d9516d3817a30bdca5e13
Abhishek Pathak <abhishekpathak1720@gmail.com>
2023-05-28 14:23:26 +0530

    tuf-day18-Check if two trees are identical or not

--- Revision/tuf/Day18.java
+++ Revision/tuf/Day18.java
@@ -6,7 +6,20 @@ public class Day18 {
 
     }
 
-    
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) {
+            return true;
+        }
+        if (p == null || q == null) {
+            return false;
+        }
+        if (p.val != q.val) {
+            return false;
+        }
+
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+
     // Lowest Common Ancestor of a Binary Tree
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
         if (root == null || root == p || root == q) {
