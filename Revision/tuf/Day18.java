b49fc008a777857b9cb4448307f3528f8d516411
Abhishek Pathak <abhishekpathak1720@gmail.com>
2023-05-28 12:20:51 +0530

    tuf-day18-Binary Tree Level Order Traversal

---
+++ Revision/tuf/Day18.java
@@ -0,0 +1,52 @@
+import java.util.*;
+
+public class Day18 {
+
+    public static void main(String[] args) {
+
+    }
+
+    // 102. Binary Tree Level Order Traversal
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> ans = new ArrayList<>();
+        Queue<TreeNode> q = new LinkedList<>();
+        if (root == null)
+            return ans;
+        q.offer(root);
+        while (!q.isEmpty()) {
+            int size = q.size();
+            List<Integer> a = new ArrayList<>();
+            for (int i = 0; i < size; i++) {
+                if (q.peek().left != null) {
+                    q.offer(q.peek().left);
+                }
+                if (q.peek().right != null) {
+                    q.offer(q.peek().right);
+                }
+                a.add(q.poll().val);
+            }
+            ans.add(a);
+        }
+        return ans;
+    }
+
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+}
