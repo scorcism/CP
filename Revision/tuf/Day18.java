34420442716156358e5f2d2d44a4e9efc4d3f40f
Abhishek Pathak <abhishekpathak1720@gmail.com>
2023-05-28 12:22:26 +0530

    tuf-day18-104. Maximum Depth of Binary Tree

--- Revision/tuf/Day18.java
+++ Revision/tuf/Day18.java
@@ -6,6 +6,14 @@ public class Day18 {
 
     }
 
+    // 104. Maximum Depth of Binary Tree
+    public int maxDepth(TreeNode root) {
+        if(root == null ){
+            return 0;
+        }
+        return (1+ Math.max(maxDepth(root.left), maxDepth(root.right)));
+    }
+
     // 102. Binary Tree Level Order Traversal
     public List<List<Integer>> levelOrder(TreeNode root) {
         List<List<Integer>> ans = new ArrayList<>();
