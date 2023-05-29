20845807ca9bdb4bcc21eab3af6f3d815d373756
Abhishek Pathak <abhishekpathak1720@gmail.com>
2023-05-29 11:35:53 +0530

    tuf-day19-Binary Tree Maximum Path Sum

---
+++ Revision/tuf/Day19.java
@@ -0,0 +1,45 @@
+public class Day19 {
+
+    public static void main(String[] args) {
+
+    }
+
+    // Binary Tree Maximum Path Sum
+    static int maxi = 0;
+
+    public int maxPathSum(TreeNode root) {
+        int[] ans = new int[1];
+        ans[0] = Integer.MAX_VALUE;
+        maxpathsum(root, ans);
+        return ans[0];
+    }
+
+    static int maxpathsum(TreeNode root, int[] ans) {
+        if (root == null) {
+            return 0;
+        }
+        int maxL = Math.max(0, maxpathsum(root.left, ans));
+        int maxR = Math.max(0, maxpathsum(root.right, ans));
+        ans[0] = Math.max(ans[0], maxL + maxR + root.val);
+        return root.val + Math.max(maxL, maxR);
+    }
+
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
