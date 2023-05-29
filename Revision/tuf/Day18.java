d4b223c96701c07823a2b5c7d70f79b7711f1d4d
Abhishek Pathak <abhishekpathak1720@gmail.com>
2023-05-29 10:54:45 +0530

    tuf-day18-Boundary Traversal of binary tree

--- Revision/tuf/Day18.java
+++ Revision/tuf/Day18.java
@@ -6,6 +6,71 @@ public class Day18 {
 
     }
 
+    // Boundary Traversal of binary tree
+    ArrayList<Integer> boundary(Node node) {
+        ArrayList<Integer> ans = new ArrayList<>();
+        if (isLeaf(node) == false) {
+            ans.add(node.data);
+            addLeftBoundry(node, ans);
+            addLeaves(node, ans);
+            addRightBoundary(node, ans);
+            return ans;
+        }
+        return ans;
+    }
+
+    private void addRightBoundary(Node node, ArrayList<Integer> ans) {
+        Node curr = node.right;
+        ArrayList<Integer> tmp = new ArrayList<>();
+        while (curr != null) {
+            if (!isLeaf(curr)) {
+                tmp.add(curr.data);
+            }
+            if (curr.right != null) {
+                curr = curr.right;
+            } else {
+                curr = curr.left;
+            }
+        }
+        for (int i = tmp.size(); i >= 0; i--) {
+            ans.add(tmp.get(i));
+        }
+    }
+
+    private void addLeaves(Node node, ArrayList<Integer> ans) {
+        if (isLeaf(node)) {
+            ans.add(node.data);
+            return;
+        }
+        if (node.left != null) {
+            addLeaves(node.left, ans);
+        }
+        if (node.right != null) {
+            addLeaves(node.right, ans);
+        }
+    }
+
+    private void addLeftBoundry(Node node, ArrayList<Integer> ans) {
+        Node curr = node.left;
+        while (curr != null) {
+            if (!isLeaf(curr)) {
+                ans.add(curr.data);
+            }
+            if (curr.left != null) {
+                curr = curr.left;
+            } else {
+                curr = curr.right;
+            }
+        }
+    }
+
+    static boolean isLeaf(Node root) {
+        if (root.left == null && root.right == null) {
+            return true;
+        }
+        return false;
+    }
+
     // Binary Tree Zigzag Level Order Traversal
     public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
 
@@ -22,16 +87,16 @@ public class Day18 {
             for (int i = 0; i < size; i++) {
                 TreeNode node = q.poll();
                 // int index = (leftToRight) ? i : (size - 1 - i);
-                if(leftToRight){
+                if (leftToRight) {
                     a.add(node.val);
-                }else{
-                    a.add(0,node.val);
+                } else {
+                    a.add(0, node.val);
                 }
                 // a.add(index, node.val);
-                if(node.left != null){
+                if (node.left != null) {
                     q.add(node.left);
                 }
-                if(node.right!= null){
+                if (node.right != null) {
                     q.add(node.right);
                 }
             }
@@ -163,6 +228,16 @@ public class Day18 {
             this.val = val;
             this.left = left;
             this.right = right;
+        }
+    }
+
+    static class Node {
+        int data;
+        Node left, right;
+
+        public Node(int d) {
+            data = d;
+            left = right = null;
         }
     }
 
