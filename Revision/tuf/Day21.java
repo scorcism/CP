c32080209b3f03e999fd6712c900cdd2f54c32bb
Abhishek Pathak <abhishekpathak1720@gmail.com>
2023-05-30 18:59:27 +0530

    tuf-day21-Floor in BST

--- Revision/tuf/Day21.java
+++ Revision/tuf/Day21.java
@@ -1,3 +1,55 @@
 public class Day21 {
-    
+
+    public static void main(String[] args) {
+
+    }
+
+    // Floor int BST
+    public static int floor(Node root, int x) {
+        int floor = -1;
+        while (root != null) {
+            if (root.data == x) {
+                return x;
+            }
+            if (root.data <= x) {
+                floor = root.data;
+                root = root.right;
+            } else {
+                root = root.left;
+            }
+        }
+        return floor;
+    }
+
+    static public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    static class Node {
+        int data;
+        Node right;
+        Node left;
+
+        Node(int val) {
+            data = val;
+            right = null;
+            left = null;
+        }
+    }
+
 }
