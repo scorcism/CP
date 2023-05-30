c3807d7a3f7bd75f35b61cbd5f91e2ff4167ae10
Abhishek Pathak <abhishekpathak1720@gmail.com>
2023-05-30 10:12:43 +0530

    tuf-day20-search in BST

---
+++ Revision/tuf/Day20.java
@@ -0,0 +1,35 @@
+public class Day20 {
+
+    // Binary Search Tree
+    public static void main(String[] args) {
+
+    }
+
+    // search in BST
+    public TreeNode search(TreeNode root, int val) {
+        while (root != null && root.val != val) {
+            root = val < root.val ? root.left : root.right;
+        }
+        return root;
+    }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+}
