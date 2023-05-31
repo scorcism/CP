b2dba0cb53396e8b704f4aa78619b06e07dfe34a
Abhishek Pathak <abhishekpathak1720@gmail.com>
2023-05-31 10:57:53 +0530

    tuf-day21-Serialize and deserialize Binary Tree

--- Revision/tuf/Day21.java
+++ Revision/tuf/Day21.java
@@ -1,9 +1,61 @@
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Stack;
 
 public class Day21 {
 
     public static void main(String[] args) {
 
+    }
+
+    static class Codec {
+
+        // Encodes a tree to a single string.
+        public String serialize(TreeNode root) {
+            if (root == null) {
+                return "";
+            }
+            StringBuilder sb = new StringBuilder();
+            Queue<TreeNode> q = new LinkedList<>();
+            q.add(root);
+
+            while (!q.isEmpty()) {
+                TreeNode top = q.poll();
+                if (top == null) {
+                    sb.append("$ ");
+                    continue;
+                }
+                sb.append(top.val + " ");
+                q.add(top.left);
+                q.add(top.right);
+            }
+            return sb.toString();
+        }
+
+        // Decodes your encoded data to tree.
+        public TreeNode deserialize(String data) {
+            if (data == "") {
+                return null;
+            }
+            Queue<TreeNode> q = new LinkedList<>();
+            String[] values = data.split(" ");
+            TreeNode root = new TreeNode(Integer.parseInt(values[0]));
+            q.add(root);
+            for (int i = 1; i < values.length; i++) {
+                TreeNode parent = q.poll();
+                if (!values[i].equals("$")) {
+                    TreeNode left = new TreeNode(Integer.parseInt(values[i]));
+                    parent.left = left;
+                    q.add(left);
+                }
+                if (!values[++i].equals("$")) {
+                    TreeNode right = new TreeNode(Integer.parseInt(values[i]));
+                    parent.right = right;
+                    q.add(right);
+                }
+            }
+            return root;
+        }
     }
 
     static class MSBNode {
