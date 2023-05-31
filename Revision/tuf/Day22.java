286613dbefa6f11db9a27949bdc34ce8ca446b35
Abhishek Pathak <abhishekpathak1720@gmail.com>
2023-05-31 13:29:15 +0530

    tuf-day22-Distinct Numbers in Window

--- Revision/tuf/Day22.java
+++ Revision/tuf/Day22.java
@@ -3,7 +3,44 @@ import java.util.*;
 public class Day22 {
 
     public static void main(String[] args) {
+        // int[] A = { 1, 2, 1, 3, 4, 2, 3 };
+        // int B = 4;
+        // System.out.println(Arrays.toString(dNums(A, B)));
 
+    }
+
+    // Distinct Numbers in Window
+    public static int[] dNums(int[] A, int B) {
+        int i = 0;
+        int j = 0;
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+        ArrayList<Integer> ans = new ArrayList<>();
+        while (j < A.length) {
+
+            // calculations
+            map.put(A[j], map.getOrDefault(A[j], 0) + 1);
+            // if window size not achieved
+            if ((j - i + 1) < B) {
+                j++;
+            }
+            // after window size is acheived
+            else if (j - i + 1 == B) {
+                ans.add(map.size());
+                map.put(A[i], map.get(A[i]) - 1);
+                if (map.get(A[i]) == 0) {
+                    map.remove(A[i]);
+                }
+                i++;
+                j++;
+            }
+        }
+
+        int[] arr = new int[ans.size()];
+        for (int p = 0; p < arr.length; p++) {
+            arr[p] = ans.get(p);
+        }
+        return arr;
     }
 
 }
