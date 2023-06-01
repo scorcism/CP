ee94801909be68295af6bfad3b80c1f3af17305b
Abhishek Pathak <abhishekpathak1720@gmail.com>
2023-06-01 09:50:02 +0530

    tuf-day23-BFS of graph

--- Revision/tuf/Day23.java
+++ Revision/tuf/Day23.java
@@ -7,6 +7,26 @@ public class Day23 {
 
     }
 
+    // BFS of graph
+    public ArrayList<Integer> bfsOfGraph(int V, ArrayList<ArrayList<Integer>> adj) {
+        ArrayList<Integer> ans = new ArrayList<>();
+        Queue<Integer> q = new LinkedList<>();
+        boolean[] vis = new boolean[V + 1];
+        q.add(0);
+        vis[0] = true;
+        while (!q.isEmpty()) {
+            int node = q.poll();
+            ans.add(node);
+            for (int it : adj.get(node)) {
+                if (!vis[it]) {
+                    vis[it] = true;
+                    q.add(it);
+                }
+            }
+        }
+        return ans;
+    }
+
     // DFS of Graph
     public ArrayList<Integer> dfsOfGraph(int V, ArrayList<ArrayList<Integer>> adj) {
         ArrayList<Integer> ans = new ArrayList<>();
