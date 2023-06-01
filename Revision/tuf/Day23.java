625196cc029023dad5e5b2ac363f666d80b10762
Abhishek Pathak <abhishekpathak1720@gmail.com>
2023-06-01 14:16:58 +0530

    tuf-day23-Topological sort || kahns alfo || cycle in graph using BFS

--- Revision/tuf/Day23.java
+++ Revision/tuf/Day23.java
@@ -7,6 +7,37 @@ public class Day23 {
 
     }
 
+    // Topological sort -> BFS
+    static int[] topoSort_KahnsAlgo_BFS(int V, ArrayList<ArrayList<Integer>> adj) {
+        int[] indegree = new int[V];
+        for (int i = 0; i < V; i++) {
+            for (int n : adj.get(i)) {
+                indegree[n]++;
+            }
+        }
+        Queue<Integer> q = new LinkedList();
+        for (int i = 0; i < V; i++) {
+            if (indegree[i] == 0) {
+                q.add(i);
+            }
+        }
+        int[] topo = new int[V];
+        int i = 0;
+        while (!q.isEmpty()) {
+            int node = q.poll();
+            topo[i++] = node;
+
+            for (int it : adj.get(node)) {
+                indegree[it]--;
+                if (indegree[it] == 0) {
+                    q.add(it);
+                }
+            }
+        }
+
+        return topo;
+    }
+
     // Topological sort -> DFS
     static int[] topoSort(int V, ArrayList<ArrayList<Integer>> adj) {
         boolean[] vis = new boolean[V];
