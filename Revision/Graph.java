7db340f4eac0da845a44624264814fde7177c63d
Abhishek Pathak <abhishekpathak1720@gmail.com>
2023-06-03 13:55:59 +0530

    graph: Shortest path in Undirected Graph having unit distance

--- Revision/Graph.java
+++ Revision/Graph.java
@@ -7,6 +7,42 @@ public class Graph {
 
     }
 
+    // Shortest path in Undirected Graph having unit distance
+    public int[] shortestPathUndirected(int[][] edges, int n, int m, int src) {
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            adj.add(new ArrayList<>());
+        }
+
+        for (int i = 0; i < m; i++) {
+            adj.get(edges[i][0]).add(edges[i][1]);
+            adj.get(edges[i][1]).add(edges[i][0]);
+        }
+
+        Queue<Integer> q = new LinkedList<>();
+        int[] dist = new int[n];
+        Arrays.fill(dist, (int) 1e9);
+        dist[src] = 0;
+        q.add(src);
+        while (!q.isEmpty()) {
+            int top = q.poll();
+
+            for (int it : adj.get(top)) {
+                if (dist[top] + 1 < dist[it]) {
+                    dist[it] = dist[top] + 1;
+                    q.add(it);
+                }
+            }
+        }
+        for (int i = 0; i < dist.length; i++) {
+            if (dist[i] == (int) 1e9) {
+                dist[i] = -1;
+            }
+        }
+        return dist;
+    }
+
     // Shortest path in Directed Acyclic Graph
     public int[] shortestPath(int N, int M, int[][] edges) {
         ArrayList<ArrayList<PairshortestPath>> adj = new ArrayList<>();
