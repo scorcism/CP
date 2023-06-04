1fcc368ee7bb1a4aea4c60f1a2dc26e2c314b887
Abhishek Pathak <abhishekpathak1720@gmail.com>
2023-06-04 19:45:59 +0530

    graph: Bellman-Ford Algorithm

--- Revision/Graph.java
+++ Revision/Graph.java
@@ -7,8 +7,37 @@ public class Graph {
 
     }
 
+    // Distance from the Source (Bellman-Ford Algorithm)
+    static int[] bellman_ford(int V, ArrayList<ArrayList<Integer>> edges, int S) {
+        int[] dist = new int[V];
+        Arrays.fill(dist, (int) 1e8);
 
-    
+        dist[S] = 0;
+
+        for (int i = 0; i < V - 1; i++) {
+            for (ArrayList<Integer> it : edges) {
+                int u = it.get(0);
+                int v = it.get(1);
+                int wt = it.get(2);
+
+                if (dist[u] + wt < dist[v]) {
+                    dist[v] = dist[u] + wt;
+                }
+            }
+        }
+        for (ArrayList<Integer> it : edges) {
+            int u = it.get(0);
+            int v = it.get(1);
+            int wt = it.get(2);
+
+            if (dist[u] + wt < dist[v]) {
+                return new int[] { -1 };
+            }
+        }
+        return dist;
+
+    }
+
     // Implementing Dijkstra Algorithm
     static int[] dijkstra(int V, ArrayList<ArrayList<ArrayList<Integer>>> adj, int S) {
         PriorityQueue<PairD> pq = new PriorityQueue<>((a, b) -> a.distance - b.distance);
