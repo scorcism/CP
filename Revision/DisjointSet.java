d0503052da99845eb508f40ef540c02fbbfcdfa2
Abhishek Pathak <abhishekpathak1720@gmail.com>
2023-06-05 12:21:04 +0530

    Disjoint Set - union by size

--- Revision/DisjointSet.java
+++ Revision/DisjointSet.java
@@ -4,11 +4,13 @@ import java.util.List;
 class Disjoint_Set {
     List<Integer> rank = new ArrayList<>();
     List<Integer> parent = new ArrayList<>();
+    List<Integer> size = new ArrayList<>();
 
     public Disjoint_Set(int n) {
         for (int i = 0; i <= n; i++) {
             rank.add(0);
             parent.add(i);
+            size.add(1);
         }
     }
 
@@ -41,6 +43,22 @@ class Disjoint_Set {
             rank.set(ulp_u, rankU + 1);
         }
     }
+
+    public void unionBySize(int u, int v) {
+        int ulp_u = findUparent(u);
+        int ulp_v = findUparent(v);
+
+        if (ulp_u == ulp_v) {
+            return;
+        }
+        if (size.get(ulp_u) < size.get(ulp_v)) {
+            parent.set(ulp_u, ulp_v);
+            size.set(ulp_v, size.get(ulp_v) + size.get(ulp_u));
+        }else{
+            parent.set(ulp_v, ulp_u);
+            size.set(ulp_u, size.get(ulp_u) + size.get(ulp_v));
+        }
+    }
 }
 
 public class DisjointSet {
@@ -48,11 +66,11 @@ public class DisjointSet {
     public static void main(String[] args) {
         Disjoint_Set ds = new Disjoint_Set(7);
 
-        ds.unionByRank(1, 2);
-        ds.unionByRank(2, 3);
-        ds.unionByRank(4, 5);
-        ds.unionByRank(6, 7);
-        ds.unionByRank(5, 6);
+        ds.unionBySize(1, 2);
+        ds.unionBySize(2, 3);
+        ds.unionBySize(4, 5);
+        ds.unionBySize(6, 7);
+        ds.unionBySize(5, 6);
 
         // 3 and 7 are same or not
         if (ds.findUparent(3) == ds.findUparent(7)) {
