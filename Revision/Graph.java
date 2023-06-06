8077486a72e0dc7404dc106929963a6177f6139d
Abhishek Pathak <abhishekpathak1720@gmail.com>
2023-06-06 11:10:37 +0530

    graph: Bridges in Graph - Using Tarjan's Algorithm of time in and low time - criticalConnections

--- Revision/Graph.java
+++ Revision/Graph.java
@@ -7,6 +7,60 @@ public class Graph {
 
     }
 
+    // Bridges in Graph - Using Tarjan's Algorithm of time in and low time
+    int timer = 0;
+
+    public List<List<Integer>> criticalConnections(int n, List<List<Integer>> connections) {
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            adj.add(new ArrayList<>());
+        }
+
+        for (List<Integer> it : connections) {
+            int u = it.get(0);
+            int v = it.get(1);
+
+            adj.get(u).add(v);
+            adj.get(v).add(u);
+        }
+
+        int[] vis = new int[n];
+        int[] tin = new int[n];
+        int[] low = new int[n];
+
+        List<List<Integer>> bridges = new ArrayList<>();
+
+        dfscriticalConnections(0, -1, vis, adj, tin, low, bridges);
+        return bridges;
+    }
+
+    private void dfscriticalConnections(int node, int parent, int[] vis, ArrayList<ArrayList<Integer>> adj, int[] tin,
+            int[] low,
+            List<List<Integer>> bridges) {
+
+        vis[node] = 1;
+        tin[node] = low[node] = 1;
+
+        for (Integer it : adj.get(node)) {
+            if (it == parent) {
+                continue;
+            }
+
+            if (vis[it] == 0) {
+
+                dfscriticalConnections(it, node, vis, adj, tin, low, bridges);
+
+                low[node] = Math.min(low[node], low[it]);
+                if(low[it] > tin[node]){
+                    bridges.add(Arrays.adList(it,node));
+                }
+            } else {
+                low[node] = Math.min(low[npde], low[it]);
+            }
+        }
+    }
+
     public int kosaraju(int V, ArrayList<ArrayList<Integer>> adj) {
         // step 1
         int vis[] = new int[V];
