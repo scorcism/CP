99634cbad2c07f64d7f34f431d5f436ca6e4c936
Abhishek Pathak <abhishekpathak1720@gmail.com>
2023-06-07 19:44:12 +0530

    V - Frequency Array

--- codeforces/Cf.java
+++ codeforces/Cf.java
@@ -1,5 +1,5 @@
 // Abhishek Pathak - scor32k
-// Date: 2023-06-07 18:49:27
+// Date: 2023-06-07 19:36:55
 
 
 import java.io.BufferedReader;
@@ -19,23 +19,18 @@ public class Cf {
         PrintWriter out = new PrintWriter(System.out);
         int n = fs.nextInt();
         int m = fs.nextInt();
-        int[] arr1 = fs.nextIntArray(n);
-        int[] arr2 = fs.nextIntArray(m);
         
-        int i = 0; 
-        int j = 0;
-        while(i < n && j < m){
-            if(arr1[i] == arr2[j]){
-                i++; j++;
-            }else if(arr1[i] != arr2[j]){
-                i++;
-            }
+        int[] arr= new int[(int)1e5];
+
+        for(int i  =0; i< n; i++){
+            int num = fs.nextInt();
+            arr[num]++;
         }
-        if(j ==m){
-            System.out.println("YES");
-        }else{
-            System.out.println("NO");
+
+        for(int i  =1; i<= m; i++){
+            System.out.println(arr[i]);
         }
+
 
         out.close();
     }
