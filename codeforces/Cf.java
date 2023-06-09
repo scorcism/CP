54df1424915f39689332125f0b6618ec62a65175
Abhishek Pathak <abhishekpathak1720@gmail.com>
2023-06-09 13:06:09 +0530

    Pascal Triangle

--- codeforces/Cf.java
+++ codeforces/Cf.java
@@ -1,5 +1,5 @@
 // Abhishek Pathak - scor32k
-// Date: 2023-06-09 12:24:02
+// Date: 2023-06-09 13:04:25
 
 
 import java.io.BufferedReader;
@@ -9,6 +9,7 @@ import java.io.PrintWriter;
 import java.io.InputStream;
 import java.util.StringTokenizer;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.Random;
@@ -17,36 +18,26 @@ public class Cf {
     public static void main(String[] args) throws Exception {
         FastScanner fs = new FastScanner(System.in);
         PrintWriter out = new PrintWriter(System.out);
-        int a = fs.nextInt();
-        int b = fs.nextInt();
-
-        
-
-        System.out.println(combinations(a, b) +" " + permutations(a, b));
-
-        out.close();
-    }
-    public static long combinations(int n, int r) {
-        long[][] dp = new long[n + 1][r + 1];
-
-        for (int i = 0; i <= n; i++) {
-            for (int j = 0; j <= Math.min(i, r); j++) {
-                if (j == 0 || j == i) {
-                    dp[i][j] = 1;
-                } else {
-                    dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j];
+        int n = fs.nextInt();
+        List<Integer> prev = null;
+        for (int i = 0; i < n; i++) {
+            List<Integer> curr = new ArrayList<>();
+            
+            for(int j = 0; j <= i; j++){
+                if(j==0 || j == i){
+                    curr.add(1);
+                }else{
+                    curr.add(prev.get(j-1)+ prev.get(j));
                 }
             }
+            prev = curr;
+            for(int q: curr){
+                System.out.print(q+" ");
+            }
+            System.out.println();
         }
 
-        return dp[n][r];
-    }
-    public static long permutations(int n, int r) {
-        long result = 1;
-        for (int i = n; i > n - r; i--) {
-            result *= i;
-        }
-        return result;
+        out.close();
     }
 
     static class FastScanner {
