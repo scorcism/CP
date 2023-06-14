1fc2c54484234bc078e19ef7c8c6ddd339e38835
Abhishek Pathak <abhishekpathak1720@gmail.com>
2023-06-14 18:54:22 +0530

    228A - Is your horseshoe on the other hoof?

--- codeforces/Cf.java
+++ codeforces/Cf.java
@@ -1,5 +1,5 @@
 // Abhishek Pathak - scor32k
-// Date: 2023-06-14 18:33:13
+// Date: 2023-06-14 18:52:53
 
 
 import java.io.BufferedReader;
@@ -12,7 +12,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Random;
 
@@ -20,51 +19,13 @@ public class Cf {
     public static void main(String[] args) throws Exception {
         FastScanner fs = new FastScanner(System.in);
         PrintWriter out = new PrintWriter(System.out);
-        String s1 = fs.next();
-        String s2 = fs.next();
-        String s3 = fs.next();
-
-        HashMap<Character,Integer> map = new HashMap<>();
-
-        boolean got = false;
-        for(char c: s3.toCharArray()){
-            map.put(c, map.getOrDefault(c, 0)+1);          
+        
+        HashSet<Integer> set = new HashSet<>();
+        for(int i = 0; i< 4; i++){
+            int k = fs.nextInt();
+            set.add(k);
         }
-        for(char c: s1.toCharArray()){
-            if(map.containsKey(c)){
-                map.put(c, map.get(c)-1);            
-            }else{
-                got = true;
-            }
-        }
-        for(char c: s2.toCharArray()){
-            if(!got){
-                if(map.containsKey(c)){
-                    map.put(c, map.get(c)-1);
-                }else{
-                    got = true;
-                    break;
-                }
-            }           
-        }
-        if(!got){
-            for(int v: map.values()){
-                if(v>0){
-                    got = true;
-                    break;
-                }
-                else if(v <0){
-                    got = true;
-                    break;
-                }
-            }
-        }
-        if(!got){
-            System.out.println("YES");
-        }else{
-            System.out.println("NO");
-        }
-
+        System.out.println(4 - set.size());
         out.close();
     }
 
