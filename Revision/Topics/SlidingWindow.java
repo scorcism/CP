299638cd2e63d4bc0f46e440d388f4a97ada162c
Abhishek Pathak <abhishekpathak1720@gmail.com>
2023-07-11 11:28:48 +0530

    lengthOfLongestSubstring

--- Revision/Topics/SlidingWindow.java
+++ Revision/Topics/SlidingWindow.java
@@ -10,30 +10,53 @@ public class SlidingWindow {
 
 class VariableWindow {
 
-    // Longest Substring Without Repeating Characters
-    /*
-     * Condition is given,
-     * window size is asked
-     * 
-     */
+    public int lengthOfLongestSubstring2(String s) {
+        int maxLen = Integer.MIN_VALUE;
 
-    /*
-     * Longest Substring Without Repeating Characters
-     * Longest Repeating Character Replacement
-     * Sliding Window Maximum
-     * Minimum Window Substring
-     * Minimum Size Subarray Sum
-     * Minimum Consecutive Cards to Pick Up
-     * Maximum Erasure Value
-     * Fruit Into Baskets
-     * Count Number of Nice Subarrays
-     * Arithmetic Slices
-     * Subarrays-with-k-different-integers
-     */
+        return maxLen;
+    }
 
 }
 
 class Tutorials {
+
+    public int lengthOfLongestSubstring(String s) {
+        // Longest Substring Without Repeating Characters
+        /*
+         * Condition is given,
+         * window size is asked
+         * 
+         */
+        int maxLen = Integer.MIN_VALUE;
+
+        int i = 0;
+        int j = 0;
+        HashMap<Character, Integer> map = new HashMap<>();
+
+        while (j < s.length()) {
+
+            map.put(s.charAt(j), map.getOrDefault(s.charAt(j), 0) + 1);
+
+            if (map.size() == j - i + 1) {
+                maxLen = Math.max(maxLen, j - i + 1);
+                j++;
+            } else if (map.size() > j - i + 1) {
+                while (map.size() < j - i + 1) {
+
+                    map.put(s.charAt(i), map.get(s.charAt(i)) - 1);
+
+                    if (map.get(s.charAt(i)) < 1) {
+                        map.remove(s.charAt(i));
+                    }
+
+                    i++;
+                }
+
+                j++;
+            }
+        }
+        return maxLen;
+    }
 
     // Longest Substring With K Unique Characters
     /*
@@ -52,12 +75,10 @@ class Tutorials {
 
             if (map.size() < k) {
                 j++;
-            }
-            else if (map.size() == k) {
-                maxLen = Math.max(maxLen, j - i+1);
+            } else if (map.size() == k) {
+                maxLen = Math.max(maxLen, j - i + 1);
                 j++;
-            }
-            else if (map.size() > k) {
+            } else if (map.size() > k) {
                 while (map.size() > k) {
                     map.put(s.charAt(i), map.get(s.charAt(i)) - 1);
                     if (map.get(s.charAt(i)) < 1) {
