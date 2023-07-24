885d2bec25a9c59fb9e16348ef1bb11aab19d210
Abhishek Pathak <abhishekpathak1720@gmail.com>
2023-07-24 08:34:36 +0530

    bs

--- Revision/Topics/BinarySearch.java
+++ Revision/Topics/BinarySearch.java
@@ -4,11 +4,28 @@ public class BinarySearch {
 
     public static void main(String[] args) {
 
+        BS bs = new BS();
+
     }
 }
 
-class BS{
+class BS {
+    public int bs(int[] nums, int k) {
+        int low = 0;
+        int high = nums.length - 1;
+        while (low <= high) {
+            i++;
+            int mid = low + ((high - low) >> 1);
 
-    
+            if (nums[mid] == k) {
+                return mid;
+            } else if (nums[mid] < k) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return -1;
+    }
+
 }
-
