e411046cc038621c27493d604c0e1ca27fa2b9c3
Abhishek Pathak <abhishekpathak1720@gmail.com>
2023-07-24 12:34:47 +0530

    875. Koko Eating Bananas

--- Revision/Topics/BinarySearch.java
+++ Revision/Topics/BinarySearch.java
@@ -6,6 +6,7 @@ public class BinarySearch {
 
         BS bs = new BS();
 
+        System.out.println(bs.minEatingSpeed(new int[] { 3, 6, 7, 11 }, 8));
     }
 }
 
@@ -26,6 +27,43 @@ class BS {
             }
         }
         return -1;
+    }
+
+    public int minEatingSpeed(int[] piles, int h) {
+
+        int ans = 0;
+        int start = 1;
+        int end = 0;
+
+        for (int n : piles) {
+            end = Math.max(end, n);
+        }
+
+        while (start <= end) {
+            int mid = start + ((end - start) >> 1);
+
+            if (isPosibleToEat(piles, h, mid)) {
+                ans = mid;
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+        return ans;
+    }
+
+    public static boolean isPosibleToEat(int[] nums, int h, int mid) {
+        int hours = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            int div = nums[i] / mid;
+            hours += div;
+            if (nums[i] % mid != 0) {
+                hours++;
+            }
+        }
+
+        return hours <= h;
     }
 
 }
