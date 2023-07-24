1215996f4f18d6494d13ddd0e308af4dfe44b85e
Abhishek Pathak <abhishekpathak1720@gmail.com>
2023-07-24 18:37:12 +0530

    Aggressive Cows

--- Revision/Topics/BinarySearch.java
+++ Revision/Topics/BinarySearch.java
@@ -10,22 +10,7 @@ public class BinarySearch {
     }
 }
 
-class Questions {
-    /*
-     * Book Allocation Problem (GFG)
-     * Aggressive cow (spoj)
-     * Prata and roti (spoj)
-     * EKO (spoj)
-     * Google kickstart A Q-3 2020
-     * 1482 Minimum Number of Days to Make m Bouquets
-     * 1283 Find the Smallest Divisor Given a Threshold
-     * 1231 Divide Chocolate
-     * 1011 Capacity To Ship Packages In N Days
-     * 875 Koko Eating Bananas Minimize
-     * 774 Max Distance to Gas Station
-     * 410 Split Array Largest Sum
-     */
-}
+
 
 class BS {
     public int bs(int[] nums, int k) {
