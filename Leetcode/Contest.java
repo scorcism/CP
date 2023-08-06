d6858cea2b584410172eaa8cbd20cff80ed278e8
Abhishek Pathak <abhishekpathak1720@gmail.com>
2023-08-06 09:20:44 +0530

    SolutionWeeklyContest357

--- Leetcode/Contest.java
+++ Leetcode/Contest.java
@@ -1,5 +1,140 @@
+import java.util.*;
+
 public class Contest {
     public static void main(String[] args) {
+
+        SolutionWeeklyContest357 sol = new SolutionWeeklyContest357();
+        ArrayList<Integer> list = new ArrayList<>();
+        // list.add(2);
+        // list.add(3);
+        // list.add(3);
+        // list.add(2);
+        // list.add(2);
+        // list.add(1);
+        // list.add(3);
+        list.add(2);
+        list.add(2);
+        list.add(1);
+        System.out.println(sol.canSplitArray2(list, 4));
+    }
+}
+
+class SolutionWeeklyContest357 {
+
+    public int maximumSafenessFactor(List<List<Integer>> grid) {
+
+        int n = grid.size();
+        int m = grid.get(0).size();
+        int count = 0;
+
+        ArrayList<Pair> al = new ArrayList<>();
+
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j< m; j++){
+                if(grid.get(i).get(j) == 1){
+                    al.add(new Pair(i, j,grid.get(i).get(j) ));
+                }
+            }
+        }
+
+        return count;
+    }
+
+    static int getManDist(Pair i, Pair j){
+        int ans=  0;
+
+        ans= Math.abs(i.i - j.i) +  Math.abs(i.j - j.j);
+
+        return ans;
+    }
+
+
+    static class Pair{
+        int i, j, num;
+        
+        Pair(int i, int j, int num){
+            this.i = i;
+            this.j = j;
+            this.num = num;
+        }
+    }
+
+    public boolean canSplitArray2(List<Integer> nums, int m) {
+        int n = nums.size();
+        int count = 1;
+
+        int totalsum = 0;
+        for (int p : nums) {
+            totalsum += p;
+        }
+        totalsum -= nums.get(0);
+
+        int i = 1;
+        while (i < n) {
+            if (totalsum >= m) {
+                count++;
+                i++;
+                totalsum -= nums.get(i);
+            }else if(totalsum < m){
+                break;
+            }
+        }
+
+        return count >= n;
+    }
+
+    public boolean canSplitArray(List<Integer> nums, int m) {
+        int n = nums.size();
+
+        int i = 0;
+        int j = 0;
+        int count = 0;
+        int sum = 0;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        while (j < n) {
+            map.put(nums.get(j), map.getOrDefault(nums.get(j), 0) + 1);
+
+            sum += nums.get(i);
+            if (j - i + 1 < 2) {
+                j++;
+
+            } else if (j - i + 1 >= 2) {
+                if (map.size() >= 1) {
+                    count++;
+                } else if (sum >= m) {
+                    count++;
+                }
+                if (map.containsKey(nums.get(i))) {
+                    map.put(nums.get(i), map.get(nums.get(i)));
+                    if (map.get(nums.get(i)) < 1) {
+                        map.remove(nums.get(i));
+                    }
+                    sum -= nums.get(i);
+                }
+
+                i++;
+                j++;
+            }
+
+        }
+        return count == n;
+    }
+
+    static void foundAndRevere(StringBuilder sb) {
+        sb.reverse();
+    }
+
+    public String finalString(String s) {
+        StringBuilder sb = new StringBuilder();
+
+        for (char c : s.toCharArray()) {
+            if (c != 'i') {
+                sb.append(c);
+            } else if (c == 'i') {
+                foundAndRevere(sb);
+            }
+        }
+        return sb.toString();
     }
 }
 
