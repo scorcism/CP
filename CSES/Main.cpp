2b4bcb583191e5ce7f2bdbaeee7d98ef7403314e
scorcism <abhishekpathak1720@gmil.com>
2023-12-10 11:27:44 +0530

    update cses

--- CSES/Main.cpp
+++ CSES/Main.cpp
@@ -7,19 +7,14 @@ int main()
     int n;
     cin >> n;
 
-    if(n == 1){
-        cout << "1";
-    }else if(n ==2 || n ==3 ){
-        cout << "NO SOLUTION";
-    }else if(n==4){
-        cout << "3 1 4 2";
-    }else{
-        for(int i = n; i> 0; i-=2){
-            cout << i << " " ;
-        }
-        for(int i = n-1; i> 0; i-=2){
-            cout << i << " " ;
-        }
+    while(n--){
+        long int y;
+        long int x;
+
+        cin >> y;
+        cin >> x;
+
+        
     }
 
     return 0;
