4dc5d533a48a92b6274951633e4026a9764440f9
scorcism <abhishekpathak1720@gmail.com>
2024-06-06 07:33:46 +0530

    valid anagram

--- plan2/week2/main.cpp
+++ plan2/week2/main.cpp
@@ -9,8 +9,10 @@ public:
     {
         unordered_set<int> set;
 
-        for(auto e: nums){
-            if(set.contains(e)){
+        for (auto e : nums)
+        {
+            if (set.contains(e))
+            {
                 return true;
             }
             set.insert(e);
@@ -18,9 +20,35 @@ public:
 
         return false;
     }
+
+    //  Q. https://leetcode.com/problems/valid-anagram/
+    bool isAnagram(string s, string t)
+    {
+        if (s.length() != t.length())
+        {
+            return false;
+        }
+
+        vector<int> freq(26, 0);
+
+        for (int i = 0; i < s.length(); i++)
+        {
+            freq[s[i] - 'a'] += 1;
+            freq[t[i] - 'a'] -= 1;
+        }
+
+        for (int i = 0; i < freq.size(); i++)
+        {
+            if (freq[i] != 0)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
 }
 
-int
 main()
 {
 
