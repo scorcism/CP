450678acf3a2269c9554972808233a99dad01ab1
scorcism <abhishekpathak1720@gmail.com>
2024-06-07 07:04:59 +0530

    two sum

--- plan2/week2/main.cpp
+++ plan2/week2/main.cpp
@@ -47,6 +47,29 @@ public:
 
         return true;
     }
+
+    // https://leetcode.com/problems/two-sum/
+    vector<int> twoSum(vector<int> &nums, int target)
+    {
+        map<int, int> m;
+        vector<int> ans;
+
+        for (int i = 0; i < nums.size(); i++)
+        {
+            if (!m.empty() && m.find(target - nums[i]) != m.end())
+            {
+                ans.push_back(i);
+                ans.push_back(m[target - nums[i]]);
+                break;
+            }
+            else
+            {
+                m[nums[i]] = i;
+            }
+        }
+        return ans;
+    }
+
 }
 
 main()
